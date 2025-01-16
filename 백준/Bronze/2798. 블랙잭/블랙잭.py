c64c6c6952c7d64e42c3fd8c0b21b31cebbe2c79
zer0ken <a37592777@gmail.com>
2025-01-16 18:26:21 +0900

    [Bronze II] Title: 블랙잭, Time: 264 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.py
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -1,23 +1,24 @@
 n, m = map(int, input().split())
-cards = tuple(sorted(filter(lambda x: x < m - 2, map(int, input().split()))))
+cards = list(sorted(filter(lambda x: x < m - 2, map(int, input().split())), reverse=True))
 
 m_ = 0
 
-for i0, v0 in list(enumerate(cards))[::-1][:-2]:
-    for i1, v1 in enumerate(cards):
+for i0 in range(n):
+    v0 = cards[i0]
+    for i1 in range(n):
         if i0 == i1:
             continue
         
-        sum_of_two = v0 + v1
+        sum_of_two = v0 + cards[i1]
         
         if sum_of_two >= m:
             continue
             
-        for i2, v2 in enumerate(cards):
+        for i2 in range(n):
             if i2 == i0 or i2 == i1:
                 continue
             
-            sum_of_three = sum_of_two + v2
+            sum_of_three = sum_of_two + cards[i2]
             
             if m >= sum_of_three > m_:
                 m_ = sum_of_three
