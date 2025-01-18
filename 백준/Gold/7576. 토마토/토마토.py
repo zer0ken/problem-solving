423d27e0e2a8ff303d8f90ec12bbef00e7f3335d
zer0ken <a37592777@gmail.com>
2025-01-18 17:01:18 +0900

    [Gold V] Title: 토마토, Time: 2952 ms, Memory: 101368 KB -BaekjoonHub

---
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -0,0 +1,46 @@
+import sys
+from collections import deque
+
+M, N = map(int, sys.stdin.readline().split())
+box = []
+queue = deque()
+left = 0
+
+for n in range(N):
+    line = []
+    raw = sys.stdin.readline().rstrip().split()
+    for m in range(M):
+        tomato = int(raw[m])
+        
+        if tomato == 0:
+            left += 1
+        elif tomato == 1:
+            queue.append((n, m))
+        line.append(tomato)
+    box.append(line)
+
+dns = (1, -1, 0, 0)
+dms = (0, 0, 1, -1)
+
+timestamp = 0
+while queue and left > 0:
+    n, m = queue.popleft()
+    timestamp = box[n][m]
+    for dn, dm in zip(dns, dms):
+        nn, nm = (n + dn, m + dm)
+        
+        if any((
+            nn < 0, nm < 0,
+            nn >= N, nm >= M
+        )):
+            continue
+          
+        if box[nn][nm] == 0:
+            box[nn][nm] = timestamp + 1
+            left -= 1
+            queue.append((nn, nm))
+            
+if left:
+    sys.stdout.write('-1')
+else:
+    sys.stdout.write(str(timestamp))
