447ac9d09da9b45e20df2277968a4c4536925301
zer0ken <a37592777@gmail.com>
2025-01-22 09:10:00 +0900

    [Silver V] Title: 나이순 정렬, Time: 192 ms, Memory: 54996 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
@@ -2,22 +2,7 @@ import sys
 readline = sys.stdin.readline
 n = int(readline().rstrip())
 
-arr = []
-for _ in range(n):
-    age, name = readline().split()
-    age = int(age)
-    
-    l, r = 0, len(arr) - 1
-    while l <= r:
-        mid = (l + r) // 2
-        mid_value = arr[mid]
-        if age < mid_value[0]:
-            r = mid - 1
-        else:
-            l = mid + 1
-    if l >= len(arr):
-        arr.append((age, name))
-    else:
-        arr.insert(l, (age, name))
+arr = [readline().split() for _ in range(n)]
+arr = sorted(arr, key=lambda x: int(x[0]))
 for age, name in arr:
     sys.stdout.write(f'{age} {name}\n')
