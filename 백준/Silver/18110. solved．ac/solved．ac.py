30df2611dc3ee7d67248903085375b3026fdbfb2
zer0ken <a37592777@gmail.com>
2025-01-25 11:52:38 +0900

    [Silver IV] Title: solved.ac, Time: 180 ms, Memory: 41980 KB -BaekjoonHub

--- 백준/Silver/18110. solved．ac/solved．ac.py
+++ 백준/Silver/18110. solved．ac/solved．ac.py
@@ -5,13 +5,13 @@ decimal.getcontext().rounding = decimal.ROUND_HALF_UP
 D = decimal.Decimal
 readline = sys.stdin.readline
 
-n = D(readline().rstrip())
+n = int(readline().rstrip())
 if n == 0:
     sys.stdout.write('0')
 else:
-    arr = sorted(D(readline().rstrip()) for _ in range(int(n)))
-    cut = int(round(D(str(n)) * D('0.15'), 0))
+    arr = sorted(int(readline().rstrip()) for _ in range(n))
+    cut = int(round(n * D('0.15'), 0))
     if cut > 0:
         arr = arr[cut:-cut]
-        n -= 2 *  cut
-    sys.stdout.write(str(round(sum(arr) / n, 0)))
+        n -= 2 * cut
+    sys.stdout.write(str(round(sum(arr) / D(str(n)), 0)))
