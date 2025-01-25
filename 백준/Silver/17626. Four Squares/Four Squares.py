5981be1e528d54250eaea0f2d6dbd1da845304e3
zer0ken <a37592777@gmail.com>
2025-01-25 13:30:41 +0900

    [Silver III] Title: Four Squares, Time: 128 ms, Memory: 109900 KB -BaekjoonHub

--- 백준/Silver/17626. Four Squares/Four Squares.py
+++ 백준/Silver/17626. Four Squares/Four Squares.py
@@ -2,18 +2,16 @@ import sys
 
 n = int(sys.stdin.readline().rstrip())
 
-dp = [0] * (n+1)
-max_sqrt = int(n ** 0.5)
-squares = [i*i for i in range(max_sqrt, 0, -1)]
+dp = [-1] * (n + 1)
 
-for sq in squares:
+max_sqrt = int(n ** 0.5)
+for i in range(max_sqrt, 0, -1):
+    sq = i * i
     dp[sq] = 1
-    for i in range(sq, n + 1):
-        if i + sq > n:
-            break
-        if dp[i] == 0:
+    for i in range(sq, n - sq + 1):
+        if dp[i] == -1:
             continue
-        if dp[i + sq] == 0:
+        if dp[i + sq] == -1:
             dp[i + sq] = dp[i] + 1
         else:
             dp[i + sq] = min(dp[i + sq], dp[i] + 1)
