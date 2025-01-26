2a77d86a7356a37952d50427f54264dd0de232e8
zer0ken <a37592777@gmail.com>
2025-01-26 13:39:07 +0900

    [Silver III] Title: N과 M (8), Time: 44 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.py
@@ -0,0 +1,18 @@
+import sys
+
+N, M = map(int, sys.stdin.readline().split())
+arr = sorted(map(int, sys.stdin.readline().split()))
+
+def dfs(stack=[], depth=0):
+    if depth == M:
+        sys.stdout.write(' '.join(map(str, stack)) + '\n')
+        return
+    depth += 1
+    for n in arr:
+        if stack and stack[-1] > n:
+            continue
+        stack.append(n)
+        dfs(stack, depth)
+        stack.pop()
+
+dfs()
