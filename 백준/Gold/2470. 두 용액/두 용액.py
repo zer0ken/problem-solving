2ec273ae5abe362c5f656c9f3425e91606cc6582
zer0ken <a37592777@gmail.com>
2025-01-30 19:44:36 +0900

    [Gold V] Title: 두 용액, Time: 152 ms, Memory: 127332 KB -BaekjoonHub

---
+++ 백준/Gold/2470. 두 용액/두 용액.py
@@ -0,0 +1,12 @@
+import sys
+import random
+input = sys.stdin.readline
+
+n, values = int(input()), list(sorted(map(int, input().split()), key=abs))
+a, b = values.pop(0), values[0]
+m = abs(a + b)
+for a_, b_ in zip(values, values[1:]):
+    m_ = abs(a_ + b_)
+    if m_ < m:
+        m, a, b = m_, a_, b_
+print(f'{min(a, b)} {max(a, b)}')
