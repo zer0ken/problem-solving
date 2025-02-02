64c51659c8f5555fe0269adaec40f72f60770dc4
zer0ken <a37592777@gmail.com>
2025-02-03 00:30:16 +0900

    [Bronze II] Title: 막대기, Time: 88 ms, Memory: 36264 KB -BaekjoonHub

---
+++ 백준/Bronze/17608. 막대기/막대기.py
@@ -0,0 +1,10 @@
+import sys
+
+N = int(sys.stdin.readline())
+stack = []
+for _ in range(N):
+    l = int(sys.stdin.readline())
+    while stack and stack[-1] <= l:
+        stack.pop()
+    stack.append(l)
+sys.stdout.write(str(len(stack)))
