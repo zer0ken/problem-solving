e8f6b223973dd150c790e82a92531e69009de73f
zer0ken <a37592777@gmail.com>
2025-02-07 11:43:03 +0900

    [Gold III] Title: 벽 부수고 이동하기 2, Time: 3824 ms, Memory: 354708 KB -BaekjoonHub

--- 백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.py
+++ 백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.py
@@ -1,43 +1,34 @@
-import sys
-from collections import deque
+def main():
+    import sys
+    from collections import deque
 
-N, M, K = map(int, sys.stdin.readline().split())
-board = []
-dist = [[[0] * (K + 1)  for __ in range(M)] for _ in range(N)]
+    N, M, K = map(int, sys.stdin.readline().split())
+    board = [list(map(int, sys.stdin.readline().rstrip())) for _ in range(N)]
+    visited = [[[0] * M for _ in range(N)] for _ in range(K + 1)]
+    visited[0][0][0] = 1
 
-for _ in range(N):
-    line = list(sys.stdin.readline().rstrip())
-    board.append(list(line))
+    deltas = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+    queue = deque([(0, 0, 0, 1)])
+    while queue:
+        row, col, broke, dist = queue.popleft()
+        if row == N - 1 and col == M - 1:
+            sys.stdout.write(str(dist))
+            break
         
-dist[0][0][0] = 1
+        for drow, dcol in deltas:
+            nrow = row + drow
+            ncol = col + dcol
+            if nrow < 0 or nrow >= N or ncol < 0 or ncol >= M:
+                continue
+            nbroke = broke + board[nrow][ncol]
+            if nbroke > K:
+                continue 
+            if not visited[nbroke][nrow][ncol]:
+                visited[nbroke][nrow][ncol] = 1
+                queue.append((nrow, ncol, nbroke, dist + 1))
+    else:
+        sys.stdout.write('-1')
 
-deltas = [
-    (0, 1, 0), (1, 0, 0), (0, -1, 0), (-1, 0, 0),
-    (0, 1, 1), (1, 0, 1), (0, -1, 1), (-1, 0, 1),
-]
-queue = deque([(0, 0, 0)])
-while queue:
-    row, col, broke = queue.popleft()
-    d = dist[row][col][broke]
-    
-    for drow, dcol, dbroke in deltas:
-        nrow = row + drow
-        ncol = col + dcol
-        nbroke = broke + dbroke
-        
-        if nrow < 0 or nrow >= N or ncol < 0 or ncol >= M or nbroke > K:
-            continue
-        if board[nrow][ncol] == '0' and dbroke:
-            continue
-        if board[nrow][ncol] == '1' and not dbroke:
-            continue
-        if dist[nrow][ncol][nbroke] == 0 or dist[nrow][ncol][nbroke] > d + 1:
-            dist[nrow][ncol][nbroke] = d + 1
-            queue.append((nrow, ncol, nbroke))
 
-d = dist[N-1][M-1]
-if sum(d) == 0:
-    sys.stdout.write('-1')
-else:
-    d = min(filter(lambda x: x != 0, d))
-    sys.stdout.write(str(d))
+if __name__ == '__main__':
+    main()
