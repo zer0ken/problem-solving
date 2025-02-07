04fd4ee6246f0b9794d55cd4d0e9ddf98fd5d006
zer0ken <a37592777@gmail.com>
2025-02-08 01:12:50 +0900

    [Silver I] Title: 경로 찾기, Time: 80 ms, Memory: 34944 KB -BaekjoonHub

--- 백준/Silver/11403. 경로 찾기/경로 찾기.py
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.py
@@ -1,29 +1,29 @@
 def main():
     import sys
+    from collections import deque
     
     input = sys.stdin.readline
     N = int(input())
     graph = [list(map(int, input().split())) for _ in range(N)]
-    INF = float('inf')
-    dist = [[0] * N for _ in range(N)]
+    path = [[0] * N for _ in range(N)]
+    visited = [0] * N
     
     for i in range(N):
+        visited = [0] * N
+        queue = deque()
         for j in range(N):
             if graph[i][j]:
-                dist[i][j] = 1
-            else:
-                dist[i][j] = INF
-    
-    for k in range(N):
-        for i in range(N):
+                queue.append(j)
+                visited[j] = 1
+                path[i][j] = 1
+        while queue:
+            k = queue.popleft()
             for j in range(N):
-                if dist[i][j] > dist[i][k] + dist[k][j]:
-                    dist[i][j] = dist[i][k] + dist[k][j]
-    
-    for i in range(N):
-        for j in range(N):
-            sys.stdout.write('1 ' if dist[i][j] != INF else '0 ')
-        sys.stdout.write('\n')
+                if graph[k][j] and not visited[j]:
+                    queue.append(j)
+                    visited[j] = 1
+                    path[i][j] = 1
+        sys.stdout.write(' '.join(map(str, path[i])) + '\n')
 
 
 if __name__ == '__main__':
