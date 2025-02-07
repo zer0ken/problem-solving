3d0a081ed297e087b6a7fc14568c48db895a30e0
zer0ken <a37592777@gmail.com>
2025-02-08 01:44:11 +0900

    [Silver I] Title: 단지번호붙이기, Time: 56 ms, Memory: 34984 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,38 +1,33 @@
-def main():
-    import sys
-    from collections import deque
-    
-    input = sys.stdin.readline
-    N = int(input())
-    board = [list(map(int, input().rstrip())) for _ in range(N)]
-    visited= [[0] * N for _ in range(N)]
-    deltas = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-    
-    groups = []
-    for i in range(N):
-        for j in range(N):
-            if visited[i][j] or not board[i][j]:
-                continue
-            visited[i][j] = 1
-            count = 0
-            queue = deque([(i, j)])
-            while queue:
-                row, col = queue.popleft()
-                count += 1
-                for drow, dcol in deltas:
-                    nrow = row + drow
-                    ncol = col + dcol
-                    if nrow < 0 or nrow >= N or ncol < 0 or ncol >= N:
-                        continue
-                    if visited[nrow][ncol] or not board[nrow][ncol]:
-                        continue
-                    visited[nrow][ncol] = 1
-                    queue.append((nrow, ncol))
-            groups.append(count)
-    
-    sys.stdout.write(f'{len(groups)}\n')
-    sys.stdout.write('\n'.join(map(str, sorted(groups))))
+import sys
+from collections import deque
 
+input = sys.stdin.readline
+N = int(input())
+board = [list(map(int, input().rstrip())) for _ in range(N)]
+visited = [[False] * N for _ in range(N)]
+deltas = [(0, 1), (1, 0), (0, -1), (-1, 0)]
 
-if __name__ == '__main__':
-    main()
+groups = []
+for i in range(N):
+    for j in range(N):
+        if visited[i][j] or not board[i][j]:
+            continue
+        visited[i][j] = True
+        count = 0
+        queue = deque([(i, j)])
+        while queue:
+            row, col = queue.popleft()
+            count += 1
+            for drow, dcol in deltas:
+                nrow = row + drow
+                ncol = col + dcol
+                if nrow < 0 or nrow >= N or ncol < 0 or ncol >= N:
+                    continue
+                if visited[nrow][ncol] or not board[nrow][ncol]:
+                    continue
+                visited[nrow][ncol] = True
+                queue.append((nrow, ncol))
+        groups.append(count)
+
+sys.stdout.write(f'{len(groups)}\n')
+sys.stdout.write('\n'.join(map(str, sorted(groups))))
