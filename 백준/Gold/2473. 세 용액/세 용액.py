9af6794e4a0d98a9e7c24ce97871f76f5cb612ce
zer0ken <a37592777@gmail.com>
2025-02-07 21:12:56 +0900

    [Gold III] Title: 세 용액, Time: 144 ms, Memory: 110740 KB -BaekjoonHub

--- 백준/Gold/2473. 세 용액/세 용액.py
+++ 백준/Gold/2473. 세 용액/세 용액.py
@@ -1,32 +1,27 @@
-def main():
-    import sys
-    
-    N, *arr = map(int, sys.stdin.read().split())
-    arr.sort()
-    
-    min_set = None
-    min_value = float('inf')
-    
-    for i in range(N - 2):
-        l = i + 1
-        r = N - 1
-        
-        while l < r:
-            value = arr[i] + arr[l] + arr[r]
-            
-            if min_value > abs(value):
-                min_value = abs(value)
-                min_set = [arr[i], arr[l], arr[r]]
-            if value == 0:
-                print(*min_set)
-                return
-            elif value < 0:
-                l += 1
-            else:
-                r -= 1
-    
-    print(*min_set)
-    
+import sys
 
-if __name__ == '__main__':
-    main()
+N = int(sys.stdin.readline())
+arr = sorted(list(map(int, sys.stdin.read().split())))
+
+min_set = []
+min_value = float('inf')
+
+for i in range(N - 2):
+    l = i + 1
+    r = N - 1
+    
+    while l < r:
+        value = arr[i] + arr[l] + arr[r]
+        
+        if min_value > abs(value):
+            min_value = abs(value)
+            min_set = [arr[i], arr[l], arr[r]]
+        if value < 0:
+            l += 1
+        elif value > 0:
+            r -= 1
+        else:
+            print(*min_set)
+            exit(0)
+
+print(*min_set)
