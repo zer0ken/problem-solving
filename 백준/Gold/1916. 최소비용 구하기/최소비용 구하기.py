f9ad421f07cc4b524b727ecdc5b1d1f3956a2023
zer0ken <a37592777@gmail.com>
2025-02-07 21:14:37 +0900

    [Gold V] Title: 최소비용 구하기, Time: 236 ms, Memory: 121460 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.py
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.py
@@ -1,33 +1,28 @@
-def main():
-    import sys
-    import heapq
-    
-    N = int(sys.stdin.readline())
-    M = int(sys.stdin.readline())
+import sys
+import heapq
 
-    buses = {i: [(i, 0)] for i in range(1, N + 1)}
-    for _ in range(M):
-        start, end, cost = map(int, sys.stdin.readline().split())
-        buses[start].append((end, cost))
-    
-    departure, arrival = map(int, sys.stdin.readline().split())
-    
-    visited = [0] * (N + 1)
-    heap = [(0, departure)]
-    dist_to = [0 if i == departure else -1 for i in range(N + 1)]
-    while heap:
-        cost, here = heapq.heappop(heap)
-        if visited[here]:
-            continue
-        visited[here] = 1
-        
-        for there, next_cost in buses[here]:
-            if dist_to[there] == -1 or dist_to[there] > cost + next_cost:
-                dist_to[there] = cost + next_cost
-                heapq.heappush(heap, (dist_to[there], there))
-    
-    sys.stdout.write(str(dist_to[arrival]))
+N = int(sys.stdin.readline())
+M = int(sys.stdin.readline())
 
+buses = {i: [(i, 0)] for i in range(1, N + 1)}
+for _ in range(M):
+    start, end, cost = map(int, sys.stdin.readline().split())
+    buses[start].append((end, cost))
 
-if __name__ == '__main__':
-    main()
+departure, arrival = map(int, sys.stdin.readline().split())
+
+visited = [0] * (N + 1)
+heap = [(0, departure)]
+dist_to = [0 if i == departure else -1 for i in range(N + 1)]
+while heap:
+    cost, here = heapq.heappop(heap)
+    if visited[here]:
+        continue
+    visited[here] = 1
+    
+    for there, next_cost in buses[here]:
+        if dist_to[there] == -1 or dist_to[there] > cost + next_cost:
+            dist_to[there] = cost + next_cost
+            heapq.heappush(heap, (dist_to[there], there))
+
+sys.stdout.write(str(dist_to[arrival]))
