77f1652c8e77c1e6e02402a1ad34dbe7e159710a
zer0ken <a37592777@gmail.com>
2025-02-09 03:49:52 +0900

    연습장

--- 연습장/실행/test.py
+++ 연습장/실행/test.py
@@ -1,49 +1,55 @@
 def main():
     import sys
+    from collections import deque
     
     readline = sys.stdin.readline
-    write = sys.stdout.write
     
-    INF = float('inf')
+    N, M = map(int, readline().split())
+    board = [list(map(int, readline().split())) for _ in range(N)]
+    virus_map = [[0] * M for _ in range(N)]
+    virus = []
+    empty = []
+    for i in range(N):
+        for j in range(M):
+            if board[i][j] == 2:
+                virus.append((i, j))
+                virus_map[i][j] = 1
+            if board[i][j] == 0:
+                empty.append((i, j))
     
-    for _ in range(int(readline())):
-        N, M, W = map(int, readline().split())
-        edges = []
-        for _ in range(M):
-            s, e, t = map(int, readline().split())
-            s -= 1
-            e -= 1
-            edges.append((s, e, t))
-            edges.append((e, s, t))
-        for _ in range(W):
-            s, e, t = map(int, readline().split())
-            s -= 1
-            e -= 1
-            edges.append((s, e, -t))
-
-        visited = [0] * N
-        for start in range(N):
-            if visited[start]:
-                continue
-            visited[start] = 1
-            dist = [INF] * N
-            dist[start] = 0
-            for loop in range(N):
-                for s, e, t in edges:
-                    if dist[s] < INF and dist[e] > dist[s] + t:
-                        visited[e] = 1
-                        dist[e] = dist[s] + t
-                        if loop == N - 1:
-                            write('YES\n')
-                            break
-                else:
+    combinations = []
+    for i in range(len(empty) - 2):
+        for j in range(i, len(empty) - 1):
+            for k in range(j, len(empty)):
+                combinations.append((empty[i], empty[j], empty[k]))
+    
+    max_safe = 0
+    for new_walls in combinations:
+        for i, j in new_walls:
+            board[i][j] = 1
+        
+        safe = len(empty) - 3
+        queue = deque(virus)
+        visited = [list(row) for row in virus_map]
+        deltas = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+        while queue:
+            row, col = queue.popleft()
+            for drow, dcol in deltas:
+                nrow, ncol = row + drow, col + dcol
+                if nrow < 0 or nrow >= N or ncol < 0 or ncol >= M \
+                        or board[nrow][ncol] or visited[nrow][ncol]:
                     continue
-                break
-            else:
-                continue
-            break
-        else:
-            write('NO\n')
+                visited[nrow][ncol] = True
+                queue.append((nrow, ncol))
+                safe -= 1
+        
+        for i, j in new_walls:
+            board[i][j] = 0
+
+        if max_safe < safe:
+            max_safe = safe
+    
+    sys.stdout.write(str(max_safe))
 
 
 if __name__ == '__main__':
