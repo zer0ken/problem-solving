6498c73f15189c71ab3302273e6724766e8318a8
zer0ken <a37592777@gmail.com>
2025-02-08 23:58:53 +0900

    [Gold II] Title: 트리의 지름, Time: 400 ms, Memory: 153992 KB -BaekjoonHub

---
+++ 백준/Gold/1167. 트리의 지름/트리의 지름.py
@@ -0,0 +1,55 @@
+def main():
+    import sys
+    from collections import deque
+    
+    readline = sys.stdin.readline
+    
+    N = int(readline())
+    tree = [[] for _ in range(N + 1)]
+    for _ in range(N - 1):
+        line = list(map(int, readline().split()))[:-1]
+        s = line[0]
+        for i in range(1, len(line), 2):
+            e, w = line[i], line[i + 1]
+            tree[s].append((e, w))
+            tree[e].append((s, w))
+    
+    queue = deque([(1, 0)])
+    visited = [0] * (N + 1)
+    visited[1] = 1
+    farthest = 0
+    farthest_node = 0
+    while queue:
+        here, dist = queue.popleft()
+        is_terminal = 1
+        for there, weight in tree[here]:
+            if visited[there]:
+                continue
+            is_terminal = 0
+            visited[there] = 1
+            queue.append((there, dist + weight))
+        if is_terminal and farthest < dist:
+            farthest = dist
+            farthest_node = here
+    
+    queue.append((farthest_node, 0))
+    visited = [0] * (N + 1)
+    visited[farthest_node] = 1
+    farthest = 0
+    while queue:
+        here, dist = queue.popleft()
+        is_terminal = 1
+        for there, weight in tree[here]:
+            if visited[there]:
+                continue
+            is_terminal = 0
+            visited[there] = 1
+            queue.append((there, dist + weight))
+        if is_terminal and farthest < dist:
+            farthest = dist
+            
+    sys.stdout.write(str(farthest))
+
+
+if __name__ == '__main__':
+    main()
