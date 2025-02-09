fa8e872bcd5de99ede58be68fdbe9c819fbde7f5
zer0ken <a37592777@gmail.com>
2025-02-09 10:59:27 +0900

    [Gold V] Title: 트리, Time: 56 ms, Memory: 34924 KB -BaekjoonHub

--- 백준/Gold/1068. 트리/트리.py
+++ 백준/Gold/1068. 트리/트리.py
@@ -1,36 +1,31 @@
-def main():
-    import sys
-    from collections import deque
-    
-    readline = sys.stdin.readline
-    write = sys.stdout.write
-    
-    N = int(readline())
-    tree = [[] for _ in range(N)]
-    root = -1
-    for node, parent in enumerate(map(int, readline().split())):
-        if parent == -1:
-            root = node
-        else:
-            tree[parent].append(node)
-    cut = int(readline())
-    if cut == root:
-        write('0')
-        exit(0)
-        
-    terminals = 0
-    queue = deque([root])
-    while queue:
-        node = queue.popleft()
-        has_child = False
-        for child in tree[node]:
-            if child != cut:
-                queue.append(child)
-                has_child = True
-        if not has_child:
-            terminals += 1
-    write(str(terminals))
+import sys
+from collections import deque
 
+readline = sys.stdin.readline
+write = sys.stdout.write
 
-if __name__ == '__main__':
-    main()
+N = int(readline())
+tree = [[] for _ in range(N)]
+root = -1
+for node, parent in enumerate(map(int, readline().split())):
+    if parent == -1:
+        root = node
+    else:
+        tree[parent].append(node)
+cut = int(readline())
+if cut == root:
+    write('0')
+    exit(0)
+    
+terminals = 0
+queue = deque([root])
+while queue:
+    node = queue.popleft()
+    has_child = False
+    for child in tree[node]:
+        if child != cut:
+            queue.append(child)
+            has_child = True
+    if not has_child:
+        terminals += 1
+write(str(terminals))
