3d4eb72e4a0756c1a86d6d48d08fc39cf2bfe91a
zer0ken <a37592777@gmail.com>
2025-02-09 11:11:11 +0900

    [Gold V] Title: 노드사이의 거리, Time: 200 ms, Memory: 34952 KB -BaekjoonHub

--- 백준/Gold/1240. 노드사이의 거리/노드사이의 거리.py
+++ 백준/Gold/1240. 노드사이의 거리/노드사이의 거리.py
@@ -1,30 +1,35 @@
-import sys
-from collections import deque
+def main():
+    import sys
+    from collections import deque
 
-readline = sys.stdin.readline
-write = sys.stdout.write
+    readline = sys.stdin.readline
+    write = sys.stdout.write
 
-N, M = map(int, readline().split())
+    N, M = map(int, readline().split())
 
-tree = [[] for _ in range(N + 1)]
-for _ in range(N - 1):
-    a, b, d = map(int, readline().split())
-    tree[a].append((b, d))
-    tree[b].append((a, d))
+    tree = [[] for _ in range(N + 1)]
+    for _ in range(N - 1):
+        a, b, d = map(int, readline().split())
+        tree[a].append((b, d))
+        tree[b].append((a, d))
 
 
-for _ in range(M):
-    start, target = map(int, readline().split())
-    
-    queue = deque([(start, 0)])
-    visited = [False] * (N + 1)
-    visited[start] = True
-    while queue:
-        here, dist = queue.popleft()
-        if here == target:
-            write(f'{dist}\n')
-            break
-        for there, delta_dist in tree[here]:
-            if not visited[there]:
-                visited[there] = True
-                queue.append((there, dist + delta_dist))
+    for _ in range(M):
+        start, target = map(int, readline().split())
+        
+        queue = deque([(start, 0)])
+        visited = [False] * (N + 1)
+        visited[start] = True
+        while queue:
+            here, dist = queue.popleft()
+            if here == target:
+                write(f'{dist}\n')
+                break
+            for there, delta_dist in tree[here]:
+                if not visited[there]:
+                    visited[there] = True
+                    queue.append((there, dist + delta_dist))
+
+
+if __name__ == '__main__':
+    main()
