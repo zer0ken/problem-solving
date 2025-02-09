2dfcd06349c5756f8fb64ae1445ed19c1b2ca931
zer0ken <a37592777@gmail.com>
2025-02-09 09:35:40 +0900

    [Silver IV] Title: 점프왕 쩰리 (Small), Time: 112 ms, Memory: 112308 KB -BaekjoonHub

--- 백준/Silver/16173. 점프왕 쩰리 （Small）/점프왕 쩰리 （Small）.py
+++ 백준/Silver/16173. 점프왕 쩰리 （Small）/점프왕 쩰리 （Small）.py
@@ -8,25 +8,21 @@ def main():
     N = int(readline())
     board = [list(map(int, readline().split())) for _ in range(N)]
     visited = [[0]*N for _ in range(N)]
-    
+    deltas = [(1, 0), (0, 1)]
     queue = deque([(0, 0)])
     while queue:
         row, col = queue.popleft()
-        
         if row == col == N - 1:
             write('HaruHaru')
             break
         
         stride = board[row][col]
-        for drow, dcol in ((stride, 0), (0, stride)):
-            nrow = row + drow
-            ncol = col + dcol
-            
-            if nrow < 0 or nrow >= N or ncol < 0 or ncol >= N or visited[nrow][ncol]:
-                continue
-            
-            visited[nrow][ncol] = 1
-            queue.append((nrow, ncol))
+        for drow, dcol in deltas:
+            nrow = row + drow * stride
+            ncol = col + dcol * stride
+            if 0 <= nrow < N and 0 <= ncol < N and not visited[nrow][ncol]:
+                visited[nrow][ncol] = 1
+                queue.append((nrow, ncol))
     else:
         write('Hing')
 
