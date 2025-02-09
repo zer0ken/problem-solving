12def747187b90e75492ab16ea43dfc0a60a6b70
zer0ken <a37592777@gmail.com>
2025-02-09 09:36:09 +0900

    [Silver IV] Title: 점프왕 쩰리 (Small), Time: 60 ms, Memory: 34944 KB -BaekjoonHub

--- 백준/Silver/16173. 점프왕 쩰리 （Small）/점프왕 쩰리 （Small）.py
+++ 백준/Silver/16173. 점프왕 쩰리 （Small）/점프왕 쩰리 （Small）.py
@@ -1,31 +1,26 @@
-def main():
-    import sys
-    from collections import deque
-    
-    readline = sys.stdin.readline
-    write = sys.stdout.write
-    
-    N = int(readline())
-    board = [list(map(int, readline().split())) for _ in range(N)]
-    visited = [[0]*N for _ in range(N)]
-    deltas = [(1, 0), (0, 1)]
-    queue = deque([(0, 0)])
-    while queue:
-        row, col = queue.popleft()
-        if row == col == N - 1:
-            write('HaruHaru')
-            break
-        
-        stride = board[row][col]
-        for drow, dcol in deltas:
-            nrow = row + drow * stride
-            ncol = col + dcol * stride
-            if 0 <= nrow < N and 0 <= ncol < N and not visited[nrow][ncol]:
-                visited[nrow][ncol] = 1
-                queue.append((nrow, ncol))
-    else:
-        write('Hing')
+import sys
+from collections import deque
 
+readline = sys.stdin.readline
+write = sys.stdout.write
 
-if __name__ == '__main__':
-    main()
+N = int(readline())
+board = [list(map(int, readline().split())) for _ in range(N)]
+visited = [[False]*N for _ in range(N)]
+deltas = [(1, 0), (0, 1)]
+queue = deque([(0, 0)])
+while queue:
+    row, col = queue.popleft()
+    if row == col == N - 1:
+        write('HaruHaru')
+        break
+    
+    stride = board[row][col]
+    for drow, dcol in deltas:
+        nrow = row + drow * stride
+        ncol = col + dcol * stride
+        if 0 <= nrow < N and 0 <= ncol < N and not visited[nrow][ncol]:
+            visited[nrow][ncol] = True
+            queue.append((nrow, ncol))
+else:
+    write('Hing')
