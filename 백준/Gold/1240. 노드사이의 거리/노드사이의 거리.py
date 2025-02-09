18eb41626c717c3979b460374551d92aef091020
zer0ken <a37592777@gmail.com>
2025-02-09 11:10:21 +0900

    [Gold V] Title: 노드사이의 거리, Time: 360 ms, Memory: 34936 KB -BaekjoonHub

---
+++ 백준/Gold/1240. 노드사이의 거리/노드사이의 거리.py
@@ -0,0 +1,30 @@
+import sys
+from collections import deque
+
+readline = sys.stdin.readline
+write = sys.stdout.write
+
+N, M = map(int, readline().split())
+
+tree = [[] for _ in range(N + 1)]
+for _ in range(N - 1):
+    a, b, d = map(int, readline().split())
+    tree[a].append((b, d))
+    tree[b].append((a, d))
+
+
+for _ in range(M):
+    start, target = map(int, readline().split())
+    
+    queue = deque([(start, 0)])
+    visited = [0] * (N + 1)
+    visited[start] = 1
+    while queue:
+        here, dist = queue.popleft()
+        if here == target:
+            write(f'{dist}\n')
+            break
+        for there, delta_dist in tree[here]:
+            if not visited[there]:
+                visited[there] = 1
+                queue.append((there, dist + delta_dist))
