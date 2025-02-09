c5f756aafe88e8952629b043ab911066d0b44aba
zer0ken <a37592777@gmail.com>
2025-02-09 19:50:06 +0900

    연습장

--- 연습장/실행/test.py
+++ 연습장/실행/test.py
@@ -1,32 +1,39 @@
 def main():
     import sys
-    
-    sys.setrecursionlimit(int(1e+4))
+    import re
+    from collections import deque
+
     readline = sys.stdin.readline
     write = sys.stdout.write
+    
+    N, start = readline().split()
+    N = int(N)
+    id_to_word = []
+    group_by_len = {}
+    for i in range(N):
+        word = readline().rstrip()
+        l = len(word)
+        if l in group_by_len:
+            group_by_len[l].add(i)
+        else:
+            group_by_len[l] = {i}
+        id_to_word[i] = word
 
-    N, M = map(int, readline().split())
-    board = [list(map(int, readline().split())) for _ in range(N + 1)]
-    deltas = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-    dp = [[-1] * M for _ in range(N)]
-    dp[-1][-1] = 1
+    queue = deque([start])
+    visited = [0] * N
+    while queue:
+        word = queue.popleft()
+        l = len(word)
+        if l + 1 not in group_by_len:
+            write(word)
+            exit(0)
+        p = re.compile('^.?' + '.?'.join(word) + '.?$')
+        for next_word in group_by_len[l + 1]:
+            if visited[l + 1] and p.match(id_to_word[next_word]):
+                visited[l + 1].add(next_word)
+                queue.append(next_word)
     
-    def dfs(row, col, dp):
-        if dp[row][col] != -1:
-            return dp[row][col]
-        cases = 0
-        height = board[row][col]
-        for drow, dcol in deltas:
-            nrow, ncol = row + drow, col + dcol
-            if 0 <= nrow < N and 0 <= ncol < M and board[nrow][ncol] < height:
-                if dp[nrow][ncol] != -1:
-                    cases += dp[nrow][ncol]
-                else:
-                    cases += dfs(nrow, ncol, dp)
-        dp[row][col] = cases
-        return cases
-    
-    write(f'{dfs(0, 0, dp)}')
+    write(word)
 
 
 if __name__ == '__main__':
