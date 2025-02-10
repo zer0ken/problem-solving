9256bb979503c053754bbf77cac5814de1984b87
zer0ken <a37592777@gmail.com>
2025-02-10 13:03:28 +0900

    [Gold V] Title: 물병, Time: 1772 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/1052. 물병/물병.py
+++ 백준/Gold/1052. 물병/물병.py
@@ -5,29 +5,10 @@ def main():
     write = sys.stdout.write
     
     N, K = map(int, readline().split())
-    bottles = N
     purchased = 0
-    
-    counter = [N]
-    while True:
-        i = 0
-        while i < len(counter):
-            merged = counter[i] // 2
-            left = counter[i] % 2
-            if not merged:
-                break
-            bottles = bottles - counter[i] + left + merged
-            counter[i] = left
-            if i == len(counter) - 1:
-                counter.append(merged)
-            else:
-                counter[i+1] += merged
-            i += 1
-        if bottles <= K:
-            break
-        bottles += 1
+    while bin(N).count('1') > K:
+        N += 1
         purchased += 1
-        counter[0] += 1
 
     write(str(purchased))
 
