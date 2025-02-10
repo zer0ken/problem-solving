a27769cdb68476f37348ca7468c1b8a233a7d16c
zer0ken <a37592777@gmail.com>
2025-02-10 09:57:25 +0900

    [Gold V] Title: 토달기, Time: 136 ms, Memory: 114384 KB -BaekjoonHub

--- 백준/Gold/1897. 토달기/토달기.py
+++ 백준/Gold/1897. 토달기/토달기.py
@@ -1,15 +1,6 @@
 def main():
     import sys
-    import re
-    from collections import Counter, deque
-
-    def is_todalin(start, end):
-        for i, (s, e) in enumerate(zip(start, end)):
-            if s != e:
-                break
-        else:
-            i += 1
-        return 1 if start == end[:i] + end[i+1:] else 0
+    from collections import deque
 
     readline = sys.stdin.readline
     write = sys.stdout.write
@@ -17,6 +8,7 @@ def main():
     N, start = readline().split()
     N = int(N)
     words = [readline().rstrip() for _ in range(N)]
+    words.sort(key=len)
     queue = deque([words.index(start)])
     visited = [0] * N
     while queue:
@@ -25,11 +17,22 @@ def main():
         l = len(word)
         has_no_next = 1
         for j, next_word in enumerate(words):
-            if len(next_word) == l + 1:
-                has_no_next = 0
-                if not visited[j] and is_todalin(word, next_word):
-                    visited[j] = 1
-                    queue.append(j)
+            if len(next_word) < l + 1:
+                continue
+            if len(next_word) > l + 1:
+                break
+            has_no_next = 0
+            if visited[j]:
+                continue
+            for k, (s, e) in enumerate(zip(word, next_word)):
+                if s != e:
+                    break
+            else:
+                k += 1
+            if word != next_word[:k] + next_word[k+1:]:
+                continue
+            visited[j] = 1
+            queue.append(j)
         if has_no_next:
             break
     write(word)
