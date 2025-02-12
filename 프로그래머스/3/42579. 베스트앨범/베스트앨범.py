90041f7e3fb8cf2aef490cc2252d5ebeeabca7ef
zer0ken <a37592777@gmail.com>
2025-02-12 17:05:13 +0900

    [level 3] Title: 베스트앨범, Time: 0.07 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -1,15 +1,12 @@
 def solution(genres, plays):
     import heapq
+    from collections import defaultdict
     
-    genre_plays = {}
-    genre_songs = {}
+    genre_plays = defaultdict(int)
+    genre_songs = defaultdict(list)
     for i, (g, p) in enumerate(zip(genres, plays)):
-        if g not in genre_plays:
-            genre_plays[g] = p
-            genre_songs[g] = [(-p, i)]
-        else:
-            genre_plays[g] += p
-            heapq.heappush(genre_songs[g], (-p, i))
+        genre_plays[g] += p
+        heapq.heappush(genre_songs[g], (-p, i))
     
     playlist = []
     for g in sorted(genre_plays.keys(), key=lambda g: -genre_plays[g]):
