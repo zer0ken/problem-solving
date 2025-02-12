ea9eb065bdfd939161ceaa4f3261970494f8f319
zer0ken <a37592777@gmail.com>
2025-02-12 10:07:26 +0900

    [Bronze V] Title: 정보보호학부 동아리 소개, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/28691. 정보보호학부 동아리 소개/정보보호학부 동아리 소개.py
@@ -0,0 +1,11 @@
+c = input()
+if c == 'M':
+    print('MatKor')
+elif c == 'W':
+    print('WiCys')
+elif c == 'C':
+    print('CyKor')
+elif c == 'A':
+    print('AlKor')
+else:
+    print('$clear')
