3154e122a60f3ea1798238eb1dd917b24581c059
zer0ken <a37592777@gmail.com>
2025-02-17 00:16:27 +0900

    연습장

---
+++ 연습장/실행/선분 체인.py
@@ -0,0 +1,61 @@
+def main():
+    import sys
+    from bisect import bisect_left
+    from collections import deque
+    
+    readline = sys.stdin.readline
+    write = sys.stdout.write
+    
+    N = int(readline())
+    edges = []
+    for _ in range(N):
+        s, e = map(int, readline().split())
+        edges.append((s, e))
+    edges.sort()
+    
+    end = []
+    cross = [[] for _ in range(N)]
+    for i, (s, e) in enumerate(edges):
+        k = bisect_left(end, (e, i))
+        if k == len(end):
+            end.append((e, i))
+        else:
+            for _, j in end[k:]:
+                cross[j].append(i)
+                cross[i].append(j),
+            end.insert(k, (e, i))
+    
+    def farthest_from(here, visited, global_visited):
+        max_distance = 0
+        farthest = here
+        
+        for there in cross[here]:
+            if not visited[there]:
+                global_visited[there] = True
+                visited[there] = True
+                farthest_, distance = farthest_from(there, visited, global_visited)
+                visited[there] = False
+                
+                if max_distance < distance:
+                    max_distance = distance
+                    farthest = farthest_
+                
+        return farthest, max_distance + 1
+    
+    global_visited = [False] * N
+    max_diameter = 0
+    for i in range(N):
+        if global_visited[i]:
+            continue
+        global_visited[i] = True
+        visited = [False] * N
+        farthest, _ = farthest_from(i, visited, global_visited)
+        visited = [False] * N
+        farthest, diameter = farthest_from(farthest, visited, global_visited)
+        if max_diameter < diameter:
+            max_diameter = diameter
+    write(str(max_diameter))
+
+
+if __name__ == '__main__':
+    main()
