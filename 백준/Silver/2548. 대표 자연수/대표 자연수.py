1a9e1ffbc83fe3ad2183898e451cbab223892245
zer0ken <a37592777@gmail.com>
2025-02-17 09:37:45 +0900

    [Silver III] Title: 대표 자연수, Time: 40 ms, Memory: 34456 KB -BaekjoonHub

--- 백준/Silver/2548. 대표 자연수/대표 자연수.py
+++ 백준/Silver/2548. 대표 자연수/대표 자연수.py
@@ -1,19 +1,11 @@
-N = int(input())
-arr = list(map(int, input().split()))
-arr.sort()
+def main():
+    import sys
+    
+    N = int(sys.stdin.readline())
+    arr = list(map(int, sys.stdin.readline().split()))
+    arr.sort()
+    sys.stdout.write(str(arr[(N - 1) // 2]))
 
-acc = [0]
-for x in arr:
-    acc.append(acc[-1] + x)
 
-total_diff = float('inf')
-best = arr[0]
-for i, x in enumerate(arr):
-    new_total_diff = (i*x - acc[i]) + (acc[-1] - acc[i + 1] - (N - i - 1) * x)
-    if total_diff > new_total_diff:
-        total_diff = new_total_diff
-        best = x
-    elif total_diff < new_total_diff:
-        break 
-
-print(best)
+if __name__ == '__main__':
+    main()
