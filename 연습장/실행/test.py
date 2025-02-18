c166c234a94d925b2fb5ab5c8811ee1905b318b3
zer0ken <a37592777@gmail.com>
2025-02-18 19:56:51 +0900

    연습장

--- 연습장/실행/test.py
+++ 연습장/실행/test.py
@@ -8,19 +8,36 @@ def main():
     edges = [tuple(map(int, readline().split())) for i in range(N)]
     edges.sort()
     
-    edges_1 = []
-    edges_2 = []
+    edges1 = []
+    edges2 = []
     
     max_end = -1
     for i, (start, end) in enumerate(edges):
         if max_end > end:
-            edges_2.append(i)
+            edges2.append(i)
         else:
-            edges_1.append(i)
+            edges1.append(i)
             max_end = end
     
     
-    
+    def is_crossing(edge1, edge2):
+        s1, e1 = edges[edge1]
+        s2, e2 = edges[edge2]
+        return (s1 - s2) * (e1 - e2) < 0
+
+    p1 = 0 # pointer on edges1
+    p2 = 0 # pointer on edges2
+    chain = []
+    while p1 < len(edges1) and p2 < len(edges2):
+        edge1 = edges1[p1]
+        edge2 = edges2[p2]
+        
+        if is_crossing(edge1, edge2):
+            pass
+        elif edges1 < edges2:
+            p1 += 1
+        else:
+            p2 += 1
 
 if __name__ == '__main__':
     main()
--- 연습장/실행/선분 체인.py
+++ 연습장/실행/선분 체인.py
@@ -7,10 +7,11 @@ def main():
     
     N = int(readline())
     edges = [tuple(map(int, readline().split()))for _ in range(N)]
+    edges.sort(key=lambda x: x[1])
     edges.sort()
     
     as_arm = [None] * N
-    arms = [None] * N 
+    arm_of = [None] * N 
     end = []
     for i, (s, e) in enumerate(edges):
         k = bisect_left(end, (e, i))
@@ -19,52 +20,38 @@ def main():
         else:
             for j in range(k, len(end)):
                 _, e_ = end[j]
-                if arms[e_] is None or len(as_arm[arms[e_]]) ==  0:
+                if arm_of[e_] is None or len(as_arm[arm_of[e_]]) ==  0:
                     break
-            arms[e_] = i
+            arm_of[e_] = i
             as_arm[i] = [end[jj][1] for jj in range(j + 1, len(end))]
             if k != 0:
                 end = end[k:]
 
-    for i, interval in enumerate(edges):
-        print(f'#{i} {interval}')
-        if as_arm[i] is not None:
-            print(f'\tit is arm: {as_arm[i]}')
-        elif arms[i] is not None:
-            print(f'\tit has arm: {arms[i]}, {as_arm[arms[i]]}')
-        else:
-            print(f'\tno arm...')
-            
-    print('-'*30)
-    
     chain = 1
     max_chain = 1
     i = 0
     while i < N:
-        print(f'#{i} {edges[i]}')
-        if arms[i] is None:
+        if arm_of[i] is None:
             if max_chain < chain:
                 max_chain = chain
             chain = 1
             i += 1
-            print(f'\t.')
         else:
             best_next = None
             chain += 1
-            for next_ in as_arm[arms[i]]:
-                if best_next is None or (arms[best_next] is None and arms[next_] is not None):
+            for next_ in as_arm[arm_of[i]]:
+                if best_next is None or arm_of[best_next] is None:
                     best_next = next_
             if best_next is None:
                 i += 2
             else:
                 chain += 1
                 i = best_next
-            print(f'\t-> {best_next}')
             
     if max_chain < chain:
         max_chain = chain
         
-    print(max_chain)
+    write(str(max_chain))
 
 
 if __name__ == '__main__':
