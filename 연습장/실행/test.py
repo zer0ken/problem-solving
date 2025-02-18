4550e86bbf54b6c7a42741dd28c5fb60f4fb8d58
zer0ken <a37592777@gmail.com>
2025-02-19 08:42:35 +0900

    연습장

--- 연습장/실행/test.py
+++ 연습장/실행/test.py
@@ -1,6 +1,8 @@
 def main():
     import sys
+    from functools import cache
     
+    sys.setrecursionlimit(100000000000)
     readline = sys.stdin.readline
     write = sys.stdout.write
     
@@ -19,37 +21,50 @@ def main():
             edges1.append(i)
             max_end = end
     
-    for p1 in range(len(edges1)):
-        for p2 in range(len(edges2)):
-            get_chain(p1, p2)
-            get_chain(p1, p2, reverse=True)
-    
     
     def is_crossing(p1, p2):
+        nonlocal edges, edges1, edges2
+        
         s1, e1 = edges[edges1[p1]]
         s2, e2 = edges[edges2[p2]]
         return (s1 - s2) * (e1 - e2) < 0
-
-    def get_chain(p1, p2, reverse=False):
+    
+    
+    @cache
+    def get_chain(pp1, pp2, reverse=False):
         """
         If not reversed,
-            edges1 -> edges2 -> edges1 -> edges2 -> ...
+            (edges1 -> edges2) -> edges1 -> edges2 -> ...
         Otherwise,
-            edges2 -> edges1 -> edges2 -> edges1 -> ...
+            (edges2 -> edges1) -> edges2 -> edges1 -> ...
         """
-        on_edges1 = not reverse
-        chain = 2
-        while p1 < len(edges1) and p2 < len(edges2):
-            if on_edges1:
-                if not is_crossing(p1, p2 + 1):
-                    break
-                p2 += 1
-            elif not is_crossing(p1 + 1, p2):
-                break
-            else:
-                p1 += 1
-            chain += 1
-            on_edges1 = not on_edges1
+        nonlocal edges1, edges2
+        
+        on_edges1 = reverse
+        if on_edges1:
+            pp2 += 1
+        else:
+            pp1 += 1
+
+        if pp1 >= len(edges1) or pp2 >= len(edges2):
+            return 0
+        if is_crossing(pp1, pp2):
+            return 1 + get_chain(pp1, pp2, not on_edges1)
+        return get_chain(pp1, pp2, on_edges1)
+    
+    
+    max_chain = 1
+    p1, p2 = 0, 0
+    while p1 < len(edges1) and p2 < len(edges2):
+        if is_crossing(p1, p2):
+            max_chain = max(max_chain, 2 + get_chain(p1, p2), 2 + get_chain(p1, p2, reverse=True))
+            p1 += 1
+            p2 += 1
+        elif edges[edges1[p1]][0] < edges[edges2[p2]][0]:
+            p1 += 1
+        else:
+            p2 += 1
+    write(str(max_chain))
 
 
 if __name__ == '__main__':
