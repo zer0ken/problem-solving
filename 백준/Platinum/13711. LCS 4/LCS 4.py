224b45b1699ba1463134938ac46427aaf27168fb
zer0ken <a37592777@gmail.com>
2025-02-23 00:38:15 +0900

    [Platinum V] Title: LCS 4, Time: 204 ms, Memory: 64620 KB -BaekjoonHub

---
+++ 백준/Platinum/13711. LCS 4/LCS 4.py
@@ -0,0 +1,33 @@
+def main():
+    import sys
+    from bisect import bisect_left
+    from collections import defaultdict
+    
+    readline = sys.stdin.readline
+    write = sys.stdout.write
+    
+    N = int(readline())
+    A = list(map(int, readline().split()))
+    B = list(map(int, readline().split()))
+    
+    B_inv = defaultdict(list)
+    for i, b in enumerate(B):
+        B_inv[b].append(i)
+    
+    lis = []
+    for a in A:
+        b_inv = B_inv[a]
+        if not b_inv:
+            continue
+        for i in range(len(b_inv) - 1, -1, -1):
+            b_idx = b_inv[i]
+            idx = bisect_left(lis, b_idx)
+            if idx == len(lis):
+                lis.append(b_idx)
+            else:
+                lis[idx] = b_idx
+    write(str(len(lis)))
+
+
+if __name__ == '__main__':
+    main()
