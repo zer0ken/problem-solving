dd852fcc41b12b1b8ca79e730a1409fc380752be
zer0ken <a37592777@gmail.com>
2025-02-23 01:23:39 +0900

    연습장

--- 연습장/실행/test.py
+++ 연습장/실행/test.py
@@ -1,71 +1,31 @@
 def main():
     import sys
+    from bisect import bisect_left
+    from collections import defaultdict
     
-    sys.setrecursionlimit(100000000000)
     readline = sys.stdin.readline
     write = sys.stdout.write
     
+    A = readline().rstrip()
+    B = readline().rstrip()
     
-    N = int(readline())
-    edges = [tuple(map(int, readline().split())) for i in range(N)]
-    edges.sort()
+    B_inv = defaultdict(list)
+    for i, b in enumerate(B):
+        B_inv[b].append(i)
     
-    edges1 = []
-    edges2 = []
-    edges = [tuple(map(int, readline().split())) for i in range(N)]
-    edges.sort()
-    
-    edges1 = []
-    edges2 = []
-    
-    max_end = -1
-    for i, (start, end) in enumerate(edges):
-        if max_end > end:
-            edges2.append(i)
-        else:
-            edges1.append(i)
-            max_end = end
-    
-    
-    def is_crossing(p1, p2):
-        nonlocal edges, edges1, edges2
-        
-        s1, e1 = edges[edges1[p1]]
-        s2, e2 = edges[edges2[p2]]
-        return (s1 - s2) * (e1 - e2) < 0
-    
-    
-    def get_chain(p1, p2, on_edges1):
-        nonlocal edges1, edges2
-        
-        chain = 2
-        while p1 < len(edges1) and p2 < len(edges2):
-            if is_crossing(p1, p2):
-                chain += 1
-                on_edges1 = not on_edges1
-            if on_edges1:
-                p2 += 1
+    lis = []
+    for a in A:
+        b_inv = B_inv[a]
+        if not b_inv:
+            continue
+        for i in range(len(b_inv) - 1, -1, -1):
+            b_idx = b_inv[i]
+            idx = bisect_left(lis, b_idx)
+            if idx == len(lis):
+                lis.append(b_idx)
             else:
-                p1 += 1
-        return chain
-    
-    
-    max_chain = 1
-    p1, p2 = 0, 0
-    while p1 < len(edges1) and p2 < len(edges2) and (N - p1 - p2 > max_chain):
-        if is_crossing(p1, p2):
-            max_chain = max(
-                max_chain, 
-                get_chain(p1, p2 + 1, on_edges1=True), 
-                get_chain(p1 + 1, p2, on_edges1=False)
-            )
-            p1 += 1
-            p2 += 1
-        elif edges[edges1[p1]][0] < edges[edges2[p2]][0]:
-            p1 += 1
-        else:
-            p2 += 1
-    write(str(max_chain))
+                lis[idx] = b_idx
+    write(str(len(lis)))
 
 
 if __name__ == '__main__':
