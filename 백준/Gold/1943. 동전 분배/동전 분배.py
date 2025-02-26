2926a6776c30a42e1099040c8ae6490f4ac84e66
zer0ken <a37592777@gmail.com>
2025-02-26 22:35:05 +0900

    [Gold II] Title: 동전 분배, Time: 164 ms, Memory: 120360 KB -BaekjoonHub

--- 백준/Gold/1943. 동전 분배/동전 분배.py
+++ 백준/Gold/1943. 동전 분배/동전 분배.py
@@ -5,39 +5,41 @@ def main():
     write = sys.stdout.write
     T, F = 1, 0
     
-    for _ in range(3):
+    def solve():
         N = int(readline())
         coins = [tuple(map(int, readline().split())) for _ in range(N)]
         total = sum(p * c for p, c in coins)
+        
         if total % 2:
             write('0\n')
-            continue
+            return
         
         MAX = total // 2
-        if any(p > MAX for p, _ in coins):
-            write('0\n')
-            continue
-        
-        is_possible = [F] * (MAX + 1)
-        for price, count in coins:
-            for p in range(MAX, 0, -1):
-                if is_possible[p]:
-                    for c in range(1, count + 1):
-                        x = p + price * c
-                        if x > MAX:
-                            break
-                        is_possible[x] = T
-            
-            for c in range(1, count + 1):
-                x = price * c
-                if x > MAX:
-                    break
-                is_possible[x] = T
-            
-            if is_possible[MAX]:
-                break
+        combinations = {0}
 
-        write('1\n' if is_possible[MAX] else '0\n')
+        coins.sort(key=lambda c: c[1], reverse=True)
+
+        for price, count in coins:
+            if price > MAX:
+                write('0\n')
+                return
+            
+            new_combinations = set()
+            for comb in combinations:
+                for c in range(1, count + 1):
+                    x = comb + price * c
+                    if x == MAX:
+                        write('1\n')
+                        return
+                    if x > MAX or x in combinations:
+                        break
+                    new_combinations.add(x)
+            combinations |= new_combinations
+        
+        write('0\n')
+    
+    for _ in range(3):
+        solve()
 
 
 if __name__ == '__main__':
