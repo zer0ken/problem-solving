4611b494e97a35f41903371a43b65ac309632976
zer0ken <a37592777@gmail.com>
2025-02-26 22:12:12 +0900

    [Gold II] Title: 동전 분배, Time: 568 ms, Memory: 111848 KB -BaekjoonHub

--- 백준/Gold/1943. 동전 분배/동전 분배.py
+++ 백준/Gold/1943. 동전 분배/동전 분배.py
@@ -7,32 +7,37 @@ def main():
     
     for _ in range(3):
         N = int(readline())
-        is_possible = [F] * 50001
-        total = 0
+        coins = [tuple(map(int, readline().split())) for _ in range(N)]
+        total = sum(p * c for p, c in coins)
+        if total % 2:
+            write('0\n')
+            continue
         
-        for _ in range(N):
-            price, count = map(int, readline().split())
-            if price > 50000:
-                write('0\n')
-                break
-            total += price * count
-            for p in range(49999, 0, -1):
-                if not is_possible[p]:
-                    continue
-                for c in range(1, count + 1):
-                    x = p + c*price
-                    if x > 50000:
-                        break
-                    is_possible[x] = T
+        MAX = total // 2
+        if any(p > MAX for p, _ in coins):
+            write('0\n')
+            continue
+        
+        is_possible = [F] * (MAX + 1)
+        for price, count in coins:
+            for p in range(MAX, 0, -1):
+                if is_possible[p]:
+                    for c in range(1, count + 1):
+                        x = p + price * c
+                        if x > MAX:
+                            break
+                        is_possible[x] = T
+            
             for c in range(1, count + 1):
-                x = c * price
-                if x <= 50000:
-                    is_possible[x] = T
-        else:
-            if total % 2:
-                write('0\n')
-            else:
-                write('1\n' if is_possible[total // 2] else '0\n')
+                x = price * c
+                if x > MAX:
+                    break
+                is_possible[x] = T
+            
+            if is_possible[MAX]:
+                break
+
+        write('1\n' if is_possible[MAX] else '0\n')
 
 
 if __name__ == '__main__':
