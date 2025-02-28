45d8b1f5a951643446699dec12ff027c394fb10f
zer0ken <a37592777@gmail.com>
2025-02-28 11:15:52 +0900

    [Silver V] Title: 배찬우는 배열을 좋아해, Time: 1952 ms, Memory: 210852 KB -BaekjoonHub

--- 백준/Silver/25966. 배찬우는 배열을 좋아해/배찬우는 배열을 좋아해.py
+++ 백준/Silver/25966. 배찬우는 배열을 좋아해/배찬우는 배열을 좋아해.py
@@ -1,10 +1,19 @@
-N, M, q = map(int, input().split())
-matrix = [list(map(int, input().split())) for _ in range(N)]
-for _ in range(q):
-    op, i, j, *k = map(int, input().split())
-    if op == 0:
-        matrix[i][j] = k[0]
-    else:
-        matrix[i], matrix[j] = matrix[j], matrix[i]
-for row in matrix:
-    print(' '.join(map(str, row)))
+def main():
+    import sys
+    readline = sys.stdin.readline
+    write = sys.stdout.write
+    
+    N, M, q = map(int, readline().split())
+    matrix = [list(map(int, readline().split())) for _ in range(N)]
+    for _ in range(q):
+        op, i, j, *k = map(int, input().split())
+        if op == 0:
+            matrix[i][j] = k[0]
+        else:
+            matrix[i], matrix[j] = matrix[j], matrix[i]
+    for row in matrix:
+        write(f'{" ".join(map(str, row))}\n')
+
+
+if __name__ == '__main__':
+    main()
