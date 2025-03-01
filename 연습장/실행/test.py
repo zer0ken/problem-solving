1a2bd3aa7a9670e6c9b68e7b5c2a8e3af0378d6e
zer0ken <a37592777@gmail.com>
2025-03-01 19:38:52 +0900

    연습장

--- 연습장/실행/test.py
+++ 연습장/실행/test.py
@@ -1,37 +1,72 @@
-def main():
-    import sys
-    import heapq
+def solution(n, build_frame):
+    PILLAR = 0
+    CEIL = 1
     
-    readline = sys.stdin.readline
-    write = sys.stdout.write
+    board = [[[False, False] for _ in range(n + 1)] for _ in range(n + 1)]
     
-    N, K = map(int, readline().split())
-    
-    jewels = [tuple(map(int, readline().split())) for _ in range(N)]
-    weight_limits = [int(readline()) for _ in range(K)]
-    
-    jewels.sort()
-    weight_limits.sort()
-    
-    pq = []
-    stolen = 0
-    bag = 0
+    def is_pillar_safe(x, y):
+        if y == 0:
+            return True
+        if board[x][y-1][PILLAR]:
+            return True
+        if board[x][y][CEIL]:
+            return True
+        if x != 0 and board[x-1][y][CEIL]:
+            return True
+        return False
         
-    for weight, price in jewels:
-        while bag < K and weight > weight_limits[bag]:
-            bag += 1
-            if pq:
-                stolen += -heapq.heappop(pq)
-        if bag == K:
-            break
-        heapq.heappush(pq, -price)
-    else:
-        while bag < K and pq:
-            bag += 1
-            stolen += -heapq.heappop(pq)
+    def is_ceil_safe(x, y):
+        if board[x][y-1][PILLAR]:
+            return True
+        if board[x+1][y-1][PILLAR]:
+            return True
+        if x != 0 and board[x-1][y][CEIL] and board[x+1][y][CEIL]:
+            return True
+        return False
     
-    write(str(stolen))
+    for x, y, is_ceil, is_build in build_frame:
+        if is_ceil:
+            if is_build:
+                if is_ceil_safe(x, y):
+                    board[x][y][CEIL] = True
+            else:
+                board[x][y][CEIL] = False
+                if (
+                    (board[x][y][PILLAR] and not is_pillar_safe(x, y))
+                    or (board[x+1][y][PILLAR] and not is_pillar_safe(x + 1, y))
+                    or (board[x+1][y][CEIL] and not is_ceil_safe(x + 1, y))
+                    or (x != 0 and board[x-1][y][CEIL] and not is_ceil_safe(x -1, y))
+                ):
+                    board[x][y][CEIL] = True
+        elif is_build:
+            if is_pillar_safe(x, y):
+                board[x][y][PILLAR] = True
+        else:
+            board[x][y][PILLAR] = False
+            if (
+                (board[x][y+1][PILLAR] and not is_pillar_safe(x, y + 1))
+                or (board[x][y+1][CEIL] and not is_ceil_safe(x, y + 1))
+                or (x != 0 and board[x-1][y+1] and not is_ceil_safe(x - 1, y + 1))
+            ):
+                board[x][y][PILLAR] = True
+        
+        print('===')
+        for y in range(n, -1, -1):
+            for x in range(n + 1):
+                print('|' if board[x][y][PILLAR] else '.', end='')
+                print('_' if board[x][y][CEIL] else ' ', end='')
+            print()
+            
+        print()
+        
+    structures = []
+    for x in range(n + 1):
+        for y in range(n + 1):
+            if board[x][y][PILLAR]:
+                structures.append([x, y, PILLAR])
+            if board[x][y][CEIL]:
+                structures.append([x, y, CEIL])
+        
+    return structures
 
-
-if __name__ == '__main__':
-    main()
+solution(5, [[1,0,0,1],[1,1,1,1],[2,1,0,1],[2,2,1,1],[5,0,0,1],[5,1,0,1],[3,2,1,1],[4,2,1,1]])
