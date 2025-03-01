c727c9d180f704744ab2edaa95ea40aa48a98e79
zer0ken <a37592777@gmail.com>
2025-03-02 01:51:30 +0900

    Time: 0 ms (100%), Space: 17.7 MB (55.04%) - LeetHub

--- LeetCode/Medium/0394-decode-string/0394-decode-string.py
+++ LeetCode/Medium/0394-decode-string/0394-decode-string.py
@@ -1,23 +1,18 @@
 class Solution:
-    def decode_recursively(self, string, start):
-        decoded = ''
-        k = ''
-        i = start
-        while i < len(string):
-            c = string[i]
-            if ord('0') <= ord(c) <= ord('9'):
-                k += c
-            elif c == '[':
-                substr, end = self.decode_recursively(string, i + 1)
-                decoded += substr * int(k)
-                k = ''
-                i = end
-            elif c == ']':
-                break
-            else:
-                decoded += c
-            i += 1
-        return decoded, i
-    
     def decodeString(self, string: str) -> str:
-        return self.decode_recursively(string, 0)[0]
+        k = ['']
+        s = ['']
+        for c in string:
+            if ord('0') <= ord(c) <= ord('9'):
+                k[-1] += c
+            elif c == '[':
+                k.append('')
+                s.append('')
+            elif c == ']':
+                k.pop()
+                t = s.pop() * int(k.pop())
+                s[-1] += t
+                k.append('')
+            else:
+                s[-1] += c
+        return s[-1]
