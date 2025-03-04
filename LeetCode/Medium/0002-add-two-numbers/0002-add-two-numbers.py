2376c88cca7a82fee84e3833a93bae51e7a84c92
zer0ken <a37592777@gmail.com>
2025-03-04 22:41:06 +0900

    Time: 5 ms (43.36%), Space: 17.9 MB (57.37%) - LeetHub

--- LeetCode/Medium/0002-add-two-numbers/0002-add-two-numbers.py
+++ LeetCode/Medium/0002-add-two-numbers/0002-add-two-numbers.py
@@ -7,29 +7,25 @@ class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         c = 0
         l1_ = l1
-        zero = ListNode(val=0)
         while True:
             t = l1.val + l2.val + c
             l1.val = t % 10
             c = t // 10
 
-            if l1.next is None and l2.next is None:
-                if c:
+            if c:
+                if l1.next is None and l2.next is None:
                     l1.next = ListNode(val=1)
                     break
-                else:
-                    break
-            elif l1.next is None:
-                if c:
+                elif l1.next is None:
                     l1.next = ListNode(val=0)
-                else:
-                    l1.next = l2.next
-                    break
-            elif l2.next is None:
-                if c:
+                elif l2.next is None:
                     l2.next = ListNode(val=0)
-                else:
-                    break
+            elif l1.next is None and l2.next is not None:
+                l1.next = l2.next
+                break
+            elif l1.next is None or l2.next is None:
+                break
+
             l1 = l1.next
             l2 = l2.next
         
