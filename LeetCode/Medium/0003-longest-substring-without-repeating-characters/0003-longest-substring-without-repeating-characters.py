6a0cabe6d453f659e6c3b60fa2e7157c95765d1c
zer0ken <a37592777@gmail.com>
2025-03-05 23:23:49 +0900

    Time: 11 ms (95.34%), Space: 17.7 MB (82.32%) - LeetHub

--- LeetCode/Medium/0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ LeetCode/Medium/0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -5,12 +5,14 @@ class Solution:
         p1 = 0
         p2 = 1
         max_len = 1
+        characters = {s[0]}
         while p2 < len(s):
-            if s[p2] not in s[p1:p2]:
+            if s[p2] not in characters:
+                characters.add(s[p2])
                 p2 += 1
-                l = p2 - p1
-                if max_len < l:
-                    max_len = l
+                if max_len < len(characters):
+                    max_len = len(characters)
             else:
+                characters.remove(s[p1])
                 p1 += 1
         return max_len
