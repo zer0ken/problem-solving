c6f50b80c371de2ec71d1344897ba9e024e4ad35
zer0ken <a37592777@gmail.com>
2025-03-05 15:49:01 +0900

    [Gold IV] Title: 별 찍기 - 11, Time: 104 ms, Memory: 40640 KB -BaekjoonHub

--- 백준/Gold/2448. 별 찍기 － 11/별 찍기 － 11.py
+++ 백준/Gold/2448. 별 찍기 － 11/별 찍기 － 11.py
@@ -6,11 +6,11 @@ def main():
     last = ['*', '* *', '*****']
     
     for i in range(N):
-        write(' ' * (N - i - 1))
         if i == len(last):
             gap = len(last[-1])
             last.extend(last[j] + ' '*(gap - 2*j) + last[j] for j in range(len(last)))
-        write(last[i] + ' ' * (N - i - 1) + '\n')
+        spaces = ' ' * (N - i - 1)
+        write(spaces + last[i] + spaces + '\n')
 
 if __name__ == '__main__':
     main()
