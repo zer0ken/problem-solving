5da401b78aeb99ad22c317a0e00408ff6d91ad0d
zer0ken <a37592777@gmail.com>
2025-03-05 12:11:50 +0900

    테스트

--- 테스트/test.js
+++ 테스트/test.js
@@ -1,7 +1,6 @@
-const input = require("fs").readFileSync("/dev/stdin").toString().split(' ');
+const input = require("fs").readFileSync("/dev/stdin").toString().split(" ");
 
 const a = parseInt(input[0]);
 const b = parseInt(input[1]);
-const c = parseInt(input[2]);
 
-console.log(a + b + c);
+console.log(a + b);
--- 테스트/test.py
+++ 테스트/test.py
@@ -1,8 +1,2 @@
-a, b, c, d = map(int, (input() for _ in range(4)))
-
-total = a + b + c + d
-r1 = total // 60
-r2 = total % 60
-
-print(r1)
-print(r2)
+a, b = map(int, input().split())
+print(a + b)
