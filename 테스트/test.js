97e4ea892f31e9e90e76411df298cfabdb3a1028
zer0ken <a37592777@gmail.com>
2025-03-05 16:02:57 +0900

    테스트

--- 테스트/test.js
+++ 테스트/test.js
@@ -1,5 +1,4 @@
-const stdin = process.platform === "linux" ? "/dev/stdin" : "테스트/input.txt";
-const input = require("fs").readFileSync(stdin).toString().split(" ");
+const input = require("fs").readFileSync("/dev/stdin").toString().split(" ");
 
 const a = parseInt(input[0]);
 const b = parseInt(input[1]);
--- 테스트/test.py
+++ 테스트/test.py
@@ -1,2 +1,16 @@
-a, b = map(int, input().split())
-print(a + b)
+def main():
+    import sys
+    write = sys.stdout.write
+    
+    N = int(input())
+    last = ['*', '* *', '*****']
+    
+    for i in range(N):
+        if i == len(last):
+            gap = len(last[-1])
+            last.extend(last[j] + ' '*(gap - 2*j) + last[j] for j in range(len(last)))
+        spaces = ' ' * (N - i - 1)
+        write(spaces + last[i] + spaces + '\n')
+
+if __name__ == '__main__':
+    main()
