c5ca140fa79f7ad835e86d173687c70b0ac078d9
zer0ken <a37592777@gmail.com>
2025-03-05 12:04:42 +0900

    저장

--- 테스트/test.js
+++ 테스트/test.js
@@ -1,1 +1,7 @@
-const fs = require('fs');
+const input = require("fs").readFileSync("/dev/stdin").toString().split(' ');
+
+const a = parseInt(input[0]);
+const b = parseInt(input[1]);
+const c = parseInt(input[2]);
+
+console.log(a + b + c);
