92b0a092e6f06335f7e7fb0b09e7f4457651b248
zer0ken <a37592777@gmail.com>
2025-03-05 12:12:57 +0900

    테스트

--- 테스트/test.js
+++ 테스트/test.js
@@ -1,4 +1,5 @@
-const input = require("fs").readFileSync("/dev/stdin").toString().split(" ");
+const stdin = process.platform === "linux" ? "/dev/stdin" : "테스트/input.txt";
+const input = require("fs").readFileSync(stdin).toString().split(" ");
 
 const a = parseInt(input[0]);
 const b = parseInt(input[1]);
