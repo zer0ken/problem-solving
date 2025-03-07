cfa9856f2d350ee9a9466b8fcb54ba91f6ce50a0
zer0ken <a37592777@gmail.com>
2025-03-07 10:10:32 +0900

    테스트

--- 테스트/test.py
+++ 테스트/test.py
@@ -1,16 +1,38 @@
 def main():
     import sys
+    sys.setrecursionlimit(100000)
+    INF = float('inf')
+    read = sys.stdin.read
     write = sys.stdout.write
+
+    values = list(map(int, read().rstrip().split()))
     
-    N = int(input())
-    last = ['*', '* *', '*****']
+    tree = []
     
-    for i in range(N):
-        if i == len(last):
-            gap = len(last[-1])
-            last.extend(last[j] + ' '*(gap - 2*j) + last[j] for j in range(len(last)))
-        spaces = ' ' * (N - i - 1)
-        write(spaces + last[i] + spaces + '\n')
+    def build_tree(i, min_, max_):
+        if i == len(values) or not min_ < values[i] < max_:
+            return [], i
+        node = [values[i]]
+        i += 1
+        
+        left_node, i = build_tree(i, min_, node[0])
+        right_node, i = build_tree(i, node[0], max_)
+        
+        node.append(left_node)
+        node.append(right_node)
+        
+        return node, i
+    
+    def print_postorder_traversal(node):
+        if not node:
+            return
+        print_postorder_traversal(node[1])
+        print_postorder_traversal(node[2])
+        write(f'{node[0]}\n')
+    
+    tree, _ = build_tree(0, -INF, INF)
+    print_postorder_traversal(tree)
+
 
 if __name__ == '__main__':
     main()
