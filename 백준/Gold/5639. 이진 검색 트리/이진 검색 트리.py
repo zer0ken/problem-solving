9cd55464312c5dd1a96ad6b9646e9feead630532
zer0ken <a37592777@gmail.com>
2025-03-07 10:41:49 +0900

    [Gold IV] Title: 이진 검색 트리, Time: 56 ms, Memory: 36328 KB -BaekjoonHub

--- 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.py
+++ 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.py
@@ -7,8 +7,6 @@ def main():
 
     values = list(map(int, read().rstrip().split()))
     
-    tree = []
-    
     def build_tree(i, min_, max_):
         if i == len(values) or not min_ < values[i] < max_:
             return [], i
@@ -21,17 +19,10 @@ def main():
         node.append(left_node)
         node.append(right_node)
         
+        write(f'{node[0]}\n')
         return node, i
     
-    def print_postorder_traversal(node):
-        if not node:
-            return
-        print_postorder_traversal(node[1])
-        print_postorder_traversal(node[2])
-        write(f'{node[0]}\n')
-    
-    tree, _ = build_tree(0, -INF, INF)
-    print_postorder_traversal(tree)
+    build_tree(0, -INF, INF)
 
 
 if __name__ == '__main__':
