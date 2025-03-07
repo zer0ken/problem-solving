63c27d9e3e37546fb668e8a879cb580a4570036e
zer0ken <a37592777@gmail.com>
2025-03-07 21:28:39 +0900

    테스트

--- 테스트/test.py
+++ 테스트/test.py
@@ -1,28 +1,14 @@
 def main():
     import sys
-    sys.setrecursionlimit(100000)
-    INF = float('inf')
-    read = sys.stdin.read
+    
+    readline = sys.stdin.readline
     write = sys.stdout.write
-
-    values = list(map(int, read().rstrip().split()))
     
-    def build_tree(i, min_, max_):
-        if i == len(values) or not min_ < values[i] < max_:
-            return [], i
-        node = [values[i]]
-        i += 1
-        
-        left_node, i = build_tree(i, min_, node[0])
-        right_node, i = build_tree(i, node[0], max_)
-        
-        node.append(left_node)
-        node.append(right_node)
-        
-        write(f'{node[0]}\n')
-        return node, i
-    
-    build_tree(0, -INF, INF)
+    for _ in range(int(readline())):
+        N, M = map(int, readline().split())
+        for _ in range(M):
+            readline()
+        write(str(N - 1) + '\n')
 
 
 if __name__ == '__main__':
