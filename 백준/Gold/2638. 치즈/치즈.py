2f093d39353ea900d6caeae0fedf34aebf444b29
zer0ken <a37592777@gmail.com>
2025-03-08 18:27:07 +0900

    [Gold III] Title: 치즈, Time: 204 ms, Memory: 116184 KB -BaekjoonHub

--- 백준/Gold/2638. 치즈/치즈.py
+++ 백준/Gold/2638. 치즈/치즈.py
@@ -10,34 +10,26 @@ def main():
     cheese = sum(r.count(1) for r in board)
     neighbors = [(1, 0), (0, 1), (-1, 0), (0, -1)]
     
-    def get_cc(r, c):
-        target = board[r][c]
-        cc = [(r, c)]
+    time = 0
+    while cheese:
+        time += 1
+        
         visited = [[0] * M for _ in range(N)]
-        visited[r][c] = 1
-        queue = deque([(r, c)])
+        visited[0][0] = 1
+        queue = deque([(0, 0)])
+        exposed = set()
         while queue:
             r, c = queue.popleft()
             for dr, dc in neighbors:
                 nr, nc = r + dr, c + dc
-                if 0 <= nr < N and 0 <= nc < M and board[nr][nc] == target and not visited[nr][nc]:
-                    visited[nr][nc] = 1
-                    cc.append((nr, nc))
-                    queue.append((nr, nc))
-        return cc 
-    
-    
-    time = 0
-    while cheese:
-        time += 1
-        exposed = set()
-        for r, c in get_cc(0, 0):
-            for dr, dc in neighbors:
-                nr, nc = r + dr, c + dc
-                if 0 <= nr < N and 0 <= nc < M and board[nr][nc] != 0:
-                    if (nr, nc) not in exposed:
+                if 0 <= nr < N and 0 <= nc < M and not visited[nr][nc]:
+                    if board[nr][nc] == 0:
+                        visited[nr][nc] = 1
+                        queue.append((nr, nc))
+                    elif (nr, nc) not in exposed:
                         exposed.add((nr, nc))
                     else:
+                        visited[nr][nc] = 1
                         exposed.remove((nr, nc))
                         board[nr][nc] = 0
                         cheese -= 1
