ee331990925ddc4c6c319e78651b48d634b29e11
zer0ken <a37592777@gmail.com>
2025-03-09 12:40:34 +0900

    [Bronze III] Title: ACM 호텔, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10250. ACM 호텔/ACM 호텔.py
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.py
@@ -5,4 +5,4 @@ write = sys.stdout.write
 
 for _ in range(int(input())):
     h, _, n = map(int, readline().split())
-    write(f'{(n-1)%h + 1}{(n-1)//h + 1 :02d}\n')
+    write(f'{((n-1)%h + 1)*100 + (n-1)//h + 1}\n')
