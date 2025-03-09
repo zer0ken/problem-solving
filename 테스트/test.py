35b4ad22db8a9a324daffae35815e4863d7b6a67
zer0ken <a37592777@gmail.com>
2025-03-09 12:50:04 +0900

    테스트

--- 테스트/test.py
+++ 테스트/test.py
@@ -1,31 +1,8 @@
-def main():
-    import sys
-    from bisect import bisect_right
-    
-    readline = sys.stdin.readline
-    write = sys.stdout.write
-    
-    N = int(readline())
-    A = list(map(int, readline().split()))
-    M = int(readline())
-    B = list(map(int, readline().split()))
-    
-    dlcs = []
-    
-    for i, x in sorted(enumerate(A), key=lambda x: (-x[1], x[0])):
-        if dlcs and i < dlcs[-1][0]:
-            continue
-        j_start = dlcs[-1][1] + 1 if dlcs else 0
-        if x in B[j_start:]:
-            j = B.index(x, j_start)
-            if dlcs and j <= dlcs[-1][1]:
-                continue
-            dlcs.append((i, j, x))
+import sys
 
-    write(str(len(dlcs)))
-    if dlcs:
-        write(f'\n{" ".join(str(x) for _, _, x in dlcs)}')
-    
+readline = sys.stdin.readline
+write = sys.stdout.write
 
-if __name__ == '__main__':
-    main()
+for _ in range(int(input())):
+    h, _, n = map(int, readline().split())
+    write(str(((n-1)%h + 1)*100 + (n-1)//h + 1) + '\n')
