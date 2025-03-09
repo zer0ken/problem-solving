1fb63af74b594b6ba15f19142240900fdcb9b1f8
zer0ken <a37592777@gmail.com>
2025-03-09 23:45:49 +0900

    Time: 43 ms (32.85%), Space: 17.6 MB (82.94%) - LeetHub

--- LeetCode/Medium/0007-reverse-integer/0007-reverse-integer.py
+++ LeetCode/Medium/0007-reverse-integer/0007-reverse-integer.py
@@ -3,6 +3,8 @@ class Solution:
         if x == 0:
             return 0
         if x > 0:
-            return int(str(x)[::-1])
+            ret = int(str(x)[::-1])
+            return ret if ret <= 2**31 - 1 else 0
         if x < 0:
-            return -int(str(-x)[::-1])
+            ret = -int(str(-x)[::-1])
+            return ret if ret >= -(2**31) else 0
