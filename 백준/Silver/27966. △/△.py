c3088ab65ae175e9c615071259f4e1110f5c6101
zer0ken <a37592777@gmail.com>
2025-03-12 12:47:52 +0900

    [Silver III] Title: △, Time: 56 ms, Memory: 39856 KB -BaekjoonHub

--- 백준/Silver/27966. △/△.py
+++ 백준/Silver/27966. △/△.py
@@ -1,8 +1,14 @@
-import sys
+def main():
+    import sys
 
-N = int(sys.stdin.readline())
-sum_dist = N**2 - 2*N + 1
-results = []
-for i in range(2, N + 1):
-    results.append(f'1 {i}')
-sys.stdout.write(str(sum_dist) + '\n' + '\n'.join(results))
+    N = int(sys.stdin.readline())
+    sum_dist = N**2 - 2*N + 1
+    results = []
+    for i in range(2, N + 1):
+        results.append(f'1 {i}')
+
+    sys.stdout.write(str(sum_dist) + '\n' + '\n'.join(results))
+
+
+if __name__ == '__main__':
+    main()
