bfbc61cbed656b32cdf2ffe0e88b063dfc014515
zer0ken <a37592777@gmail.com>
2025-03-12 18:05:22 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 1800 ms, Memory: 358932 KB -BaekjoonHub

--- 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.py
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.py
@@ -1,44 +1,41 @@
 def main():
     import sys
     
-    read = sys.stdin.read
-    readline = sys.stdin.readline
-    write = sys.stdout.write
+    lines = sys.stdin.read().rstrip().splitlines()
     
-    N, M = map(int, readline().split())
+    N, M = map(int, lines[0].split())
     edges = []
-    for line in read().rstrip().splitlines():
-        a, b, c = map(int, line.split())
-        edges.append([a, b, c])
-    
-    edges.sort(key=lambda e: e[2], reverse=True)
+    for i in range(1, M + 1):
+        a, b, weight = map(int, lines[i].split())
+        edges.append([a, b, weight])
+    edges.sort(key=lambda e: -e[2])
     
     root = [x for x in range(N + 1)]
     root[0] = 1
     
     def get_root(x):
+        nonlocal root
+        
         if root[x] != x:
             root[x] = get_root(root[x])
         return root[x]
     
-    sum_c = 0
-    cc = N
-    
-    while cc > 2:
-        a, b, c = edges.pop()
+    sum_weight = 0
+    connected_components = N
+    while connected_components > 2:
+        a, b, weight = edges.pop()
         
         root_a = get_root(a)
         root_b = get_root(b)
-        
         if root_a != root_b:
             if root_a < root_b:
                 root[root_b] = root_a
             else:
                 root[root_a] = root_b
-            sum_c += c
-            cc -= 1
+            sum_weight += weight
+            connected_components -= 1
     
-    write(str(sum_c))
+    sys.stdout.write(str(sum_weight))
 
 
 if __name__ == '__main__':
