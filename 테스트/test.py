c8dc1df5df6b0ac3ddac309ac5e5d57f9617b2c4
zer0ken <a37592777@gmail.com>
2025-03-12 21:11:51 +0900

    테스트

--- 테스트/test.py
+++ 테스트/test.py
@@ -1,28 +1,53 @@
 def main():
     import sys
-    write = sys.stdout.write
 
-    lines = sys.stdin.read().rstrip().splitlines()[:-1]
-    for line in lines:
-        A, B = map(int, line.split())
-        a, b = A, B
+    write = sys.stdout.write
+    board = [list(map(int, line))
+             for line in sys.stdin.read().rstrip().splitlines()]
+    empties = [(row, col) for row in range(9)
+               for col in range(9) if board[row][col] == 0]
+    
+    blocks = [
+        [
+            [
+                (r, c) 
+                for r in range(3*row_block, 3*row_block + 3) 
+                for c in range(3*col_block, 3*col_block + 3)
+            ]
+            for col_block in range(3)
+        ]
+        for row_block in range(3)
+    ]
+
+    def solve(cur):
+        nonlocal board
         
-        a_arr = [a]
-        while a != 1:
-            a = 3*a + 1 if a % 2 else a//2
-            a_arr.append(a)
-        
-        b_arr = [b]
-        while b != 1:
-            b = 3*b + 1 if b % 2 else b//2
-            b_arr.append(b)
-        
-        for i in range(-1, -min(len(a_arr), len(b_arr)) - 1, -1):
-            if a_arr[i] != b_arr[i]:
-                i += 1
-                break
-            
-        write(f'{A} needs {len(a_arr) + i} steps, {B} needs {len(b_arr) + i} steps, they meet at {a_arr[i]}\n')
+        if len(empties) == cur:
+            for row in range(9):
+                for col in range(9):
+                    write(str(board[row][col]))
+                write('\n')
+            exit(0)
+
+        row, col = empties[cur]
+        possibles = set(range(1, 10))
+
+        possibles -= set(board[row])
+        possibles -= {board[r][col] for r in range(9)}
+
+        row_block = row // 3
+        col_block = col // 3
+        possibles -= {board[r][c] for r, c in blocks[row_block][col_block]}
+
+        if not possibles:
+            return
+
+        for p in possibles:
+            board[row][col] = p
+            solve(cur + 1)
+        board[row][col] = 0
+
+    solve(0)
 
 
 if __name__ == '__main__':
