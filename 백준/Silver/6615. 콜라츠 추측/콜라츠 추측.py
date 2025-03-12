9b0da7b341d031f6a23a38b542e68d092ced04d4
zer0ken <a37592777@gmail.com>
2025-03-12 10:14:47 +0900

    [Silver III] Title: 콜라츠 추측, Time: 64 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/6615. 콜라츠 추측/콜라츠 추측.py
+++ 백준/Silver/6615. 콜라츠 추측/콜라츠 추측.py
@@ -1,12 +1,8 @@
 def main():
     import sys
-    import heapq
-
-    readline = sys.stdin.readline
     write = sys.stdout.write
-    
+
     lines = sys.stdin.read().rstrip().splitlines()[:-1]
-    
     for line in lines:
         A, B = map(int, line.split())
         a, b = A, B
@@ -16,11 +12,17 @@ def main():
             a = 3*a + 1 if a % 2 else a//2
             a_arr.append(a)
         
-        b_step = 0
-        while b not in a_arr:
-            b_step += 1
+        b_arr = [b]
+        while b != 1:
             b = 3*b + 1 if b % 2 else b//2
-        write(f'{A} needs {a_arr.index(b)} steps, {B} needs {b_step} steps, they meet at {b}\n')
+            b_arr.append(b)
+        
+        for i in range(-1, -min(len(a_arr), len(b_arr)) - 1, -1):
+            if a_arr[i] != b_arr[i]:
+                i += 1
+                break
+            
+        write(f'{A} needs {len(a_arr) + i} steps, {B} needs {len(b_arr) + i} steps, they meet at {a_arr[i]}\n')
 
 
 if __name__ == '__main__':
