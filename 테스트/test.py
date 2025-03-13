3ea2ed9fb25a1ac6528abe0cc0baef74399d98c6
zer0ken <a37592777@gmail.com>
2025-03-13 21:53:28 +0900

    ✅

--- 테스트/test.py
+++ 테스트/test.py
@@ -2,52 +2,22 @@ def main():
     import sys
 
     write = sys.stdout.write
-    board = [list(map(int, line))
-             for line in sys.stdin.read().rstrip().splitlines()]
-    empties = [(row, col) for row in range(9)
-               for col in range(9) if board[row][col] == 0]
+    lines = iter(sys.stdin.read().rstrip().splitlines())
     
-    blocks = [
-        [
-            [
-                (r, c) 
-                for r in range(3*row_block, 3*row_block + 3) 
-                for c in range(3*col_block, 3*col_block + 3)
-            ]
-            for col_block in range(3)
-        ]
-        for row_block in range(3)
-    ]
-
-    def solve(cur):
-        nonlocal board
+    for _ in range(int(next(lines))):
+        int(next(lines))
+        kinds = list(map(int, next(lines).split()))
+        M = int(next(lines))
         
-        if len(empties) == cur:
-            for row in range(9):
-                for col in range(9):
-                    write(str(board[row][col]))
-                write('\n')
-            exit(0)
-
-        row, col = empties[cur]
-        possibles = set(range(1, 10))
-
-        possibles -= set(board[row])
-        possibles -= {board[r][col] for r in range(9)}
-
-        row_block = row // 3
-        col_block = col // 3
-        possibles -= {board[r][c] for r, c in blocks[row_block][col_block]}
-
-        if not possibles:
-            return
-
-        for p in possibles:
-            board[row][col] = p
-            solve(cur + 1)
-        board[row][col] = 0
-
-    solve(0)
+        knapsack = [0] * (M + 1)
+        knapsack[0] = 1
+    
+        for kind in kinds:
+            for i in range(M + 1):
+                if knapsack[i] != 0 and i + kind <= M:
+                    knapsack[i + kind] += knapsack[i]
+        
+        write(f'{knapsack[M]}\n')
 
 
 if __name__ == '__main__':
