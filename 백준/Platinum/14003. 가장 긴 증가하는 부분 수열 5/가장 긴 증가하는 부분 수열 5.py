8ad86d5b7f53634fd090d6350b95defe1efeab3c
zer0ken <a37592777@gmail.com>
2025-03-21 18:02:08 +0900

    [Platinum V] Title: 가장 긴 증가하는 부분 수열 5, Time: 736 ms, Memory: 285860 KB -BaekjoonHub

--- 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.py
+++ 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.py
@@ -1,34 +1,31 @@
-import sys
-from bisect import bisect_left
-from collections import deque
+def main():
+    import sys
+    from bisect import bisect_left
 
-N = int(sys.stdin.readline().rstrip())
-arr = []
-backtrack = {}
-
-for i, num in enumerate(map(int, sys.stdin.readline().split())):
-    if not arr or arr[-1][1] < num:
-        backtrack[(i, num)] = arr[-1] if arr else None
-        arr.append((i, num))
-        continue
+    N, *arr = map(int, sys.stdin.read().split())
     
-    l, r = 0, len(arr)
-    while l <= r:
-        mid = (l + r) // 2
-        if arr[mid][1] == num:
-            l = mid
-            break
-        if arr[mid][1] > num:
-            r = mid - 1
+    backtrack = {}
+    lis = []
+    indices = []
+    for i, v in enumerate(arr):
+        idx = bisect_left(lis, v)
+        if idx == len(lis):
+            lis.append(v)
+            indices.append(i)
         else:
-            l = mid + 1
-    arr[l] = (i, num)
-    backtrack[(i, num)] = arr[l - 1] if l - 1 >= 0 else None
-
-lis = deque()
-cur = arr[-1]
-while cur != None:
-    lis.appendleft(cur[1])
-    cur = backtrack[cur]
+            lis[idx] = v
+            indices[idx] = i
+        backtrack[i] = indices[idx-1] if idx != 0 else None
     
-sys.stdout.write(f'{len(arr)}\n{" ".join(map(str, lis))}')
+    lis = []
+    cur = indices[-1]
+    while cur is not None:
+        lis.append(arr[cur])
+        cur = backtrack[cur]
+    lis.reverse()
+    
+    sys.stdout.write(f'{len(lis)}\n{" ".join(map(str, lis))}')
+
+
+if __name__ == '__main__':
+    main()
