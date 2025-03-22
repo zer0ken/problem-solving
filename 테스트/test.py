70c119db76b30e0cd852d322cefaa4679eb8dd98
zer0ken <a37592777@gmail.com>
2025-03-23 01:23:06 +0900

    테스트

--- 테스트/test.py
+++ 테스트/test.py
@@ -1,33 +1,27 @@
 def main():
     import sys
+    import math
     
-    N = int(sys.stdin.read())
+    N, *abnormalities = map(int, sys.stdin.read().split())
+    acc_sum = [0]
+    for v in abnormalities:
+        acc_sum.append(acc_sum[-1] + v)
     
-    if N == 1:
-        sys.stdout.write('0')
-        exit(0)
-    
-    dp = set([N])
+    immunity_gains = [abnormalities[i] - acc_sum[i] for i in range(N)]    
+    best_gains = [immunity_gains[0]]
     for i in range(1, N):
-        next_dp = set()
-        for v in dp:
-            new_values = set()
-            if v % 3 == 0:
-                new_values.add(v // 3)
-            if v % 2 == 0:
-                new_values.add(v // 2)
-            new_values.add(v - 1)
-            
-            if 1 in new_values:
-                break
-            next_dp.update(new_values)
-        else:
-            dp = next_dp
-            continue
-        break
+        best_gains.append(immunity_gains[i] if immunity_gains[i] > best_gains[-1] else best_gains[-1])
     
-    sys.stdout.write(str(i))
-
+    immunity = 0
+    loops = 0
+    for i in range(N):
+        if immunity < acc_sum[i + 1]:
+            new_loops = math.ceil((acc_sum[i + 1] - immunity) / best_gains[i])
+            immunity += best_gains[i] * new_loops
+            loops += new_loops
+    
+    sys.stdout.write(str(loops))
+                
 
 if __name__ == '__main__':
     main()
