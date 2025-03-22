6bc5b5b5cfba490842dac194d0e6c83ce5b86fb8
zer0ken <a37592777@gmail.com>
2025-03-22 13:55:06 +0900

    [Silver IV] Title: GCD 합, Time: 148 ms, Memory: 110736 KB -BaekjoonHub

--- 백준/Silver/9613. GCD 합/GCD 합.py
+++ 백준/Silver/9613. GCD 합/GCD 합.py
@@ -1,14 +1,10 @@
 import sys
-
-def gcd(a, b):
-    if a == 0:
-        return b
-    return gcd(b % a, a)
+import math
 
 for _ in range(int(sys.stdin.readline())):
     arr = sorted(map(int, sys.stdin.readline().split()[1:]))
     acc = 0
     for i in range(len(arr) - 1):
         for j in range(i + 1, len(arr)):
-            acc += gcd(min(arr[i], arr[j]), max(arr[i], arr[j]))
+            acc += math.gcd(arr[i], arr[j])
     sys.stdout.write(f'{acc}\n')
