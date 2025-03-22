e425f5c16adfb33603f2a094370f4e9552e2bacf
zer0ken <a37592777@gmail.com>
2025-03-22 22:19:20 +0900

    [Silver III] Title: 1로 만들기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -11,15 +11,21 @@ def main():
     for i in range(1, N):
         next_dp = set()
         for v in dp:
+            new_v = set()
             if v % 3 == 0:
-                next_dp.add(v // 3)
+                new_v.add(v // 3)
             if v % 2 == 0:
-                next_dp.add(v // 2)
-            next_dp.add(v - 1)
+                new_v.add(v // 2)
+            new_v.add(v - 1)
+            next_dp.update(new_v)
+            
+            if 1 in new_v:
+                break
+        
         dp = next_dp
-
         if 1 in dp:
             break
+        
     
     sys.stdout.write(str(i))
 
