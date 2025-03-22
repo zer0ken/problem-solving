38d4b291149b6d8f8bffd78d7f5f72e4ad87092f
zer0ken <a37592777@gmail.com>
2025-03-22 21:05:34 +0900

    [Bronze V] Title: A+B - 3, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10950. A＋B － 3/A＋B － 3.py
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.py
@@ -1,1 +1,4 @@
-[print(r) for r in [sum(map(int, input().split())) for _ in range(int(input()))]]
+import sys
+
+N, *lines = sys.stdin.read().rstrip().splitlines()
+sys.stdout.write('\n'.join(str(sum(map(int, line.split()))) for line in lines))
