1c0dc5f02037fbffc97b244c802b635c239f11ec
zer0ken <a37592777@gmail.com>
2025-03-22 10:19:24 +0900

    [Silver II] Title: 좌표 압축, Time: 872 ms, Memory: 337396 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.py
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.py
@@ -1,9 +1,17 @@
-import sys
+def main():
+    import sys
+    
+    N, *arr = map(int, sys.stdin.read().split())
+    
+    compressed = {}
+    last_v = None
+    for i, v in enumerate(sorted(set(arr))):
+        if i == 0 or last_v != v:
+            compressed[v] = i
+        last_v = v
+    
+    sys.stdout.write(' '.join(str(compressed[v]) for v in arr))
 
-readline = sys.stdin.readline
 
-n = int(readline().rstrip())
-arr = list(map(int, readline().split()))
-sarr = {v: i for i, v in enumerate(sorted(set(arr)))}
-
-sys.stdout.write(' '.join([str(sarr[v]) for v in arr]))
+if __name__ == '__main__':
+    main()
