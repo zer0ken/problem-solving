4ee2f33449cc3ae1545c98ed2d5a84460b7f5379
zer0ken <a37592777@gmail.com>
2025-03-23 13:47:37 +0900

    [Silver I] Title: 정수 삼각형, Time: 68 ms, Memory: 33600 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
@@ -1,10 +1,22 @@
-import sys
+def main():
+    import sys
 
-N = int(sys.stdin.readline())
-last = [0]
-for i in range(1, N + 1):
-    cur = list(map(int, sys.stdin.readline().split()))
-    for j in range(i):
-        cur[j] += max(last[max(j - 1, 0):j + 1])
-    last = cur
-sys.stdout.write(str(max(last)))
+    N, *lines = sys.stdin.read().rstrip().splitlines()
+    
+    row = [int(lines[0])]
+    for i in range(1, int(N)):
+        next_row = list(map(int, lines[i].split()))
+        for j, v in enumerate(next_row):
+            if j == 0:
+                next_row[j] += row[j]
+            elif j == i:
+                next_row[j] += row[j-1]
+            else:
+                next_row[j] += max(row[j-1], row[j])
+        row = next_row
+
+    sys.stdout.write(str(max(row)))
+
+
+if __name__ == '__main__':
+    main()
