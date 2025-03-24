d0f0010c1a3925f72d9ae20d95bac781b56248ef
zer0ken <a37592777@gmail.com>
2025-03-24 17:33:31 +0900

    [Silver I] Title: 팩토리얼5, Time: 160 ms, Memory: 110576 KB -BaekjoonHub

--- 백준/Silver/1564. 팩토리얼5/팩토리얼5.py
+++ 백준/Silver/1564. 팩토리얼5/팩토리얼5.py
@@ -1,6 +1,5 @@
 N = int(input())
 factorial = 1
 for i in range(2, N + 1):
-    factorial = factorial * i
-    factorial = int(str(factorial).rstrip('0')[-12:])
+    factorial = int(str(factorial * i).rstrip('0')[-12:])
 print(str(factorial)[-5:])
