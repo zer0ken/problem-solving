707f431d52d46585abbbf4aff95b22385b31bcc9
zer0ken <a37592777@gmail.com>
2025-03-25 00:02:18 +0900

    [Gold IV] Title: 가장 긴 증가하는 부분 수열 4, Time: 64 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.py
+++ 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.py
@@ -1,27 +1,19 @@
 def main():
     import sys
-    from bisect import bisect_left
 
     N, *arr = map(int, sys.stdin.read().split())
 
     dp = [1] * N
+    lis = [[arr[i]] for i in range(N)]
     
     for i in range(1, N):
         for j in range(i):
             if arr[j] < arr[i] and dp[i] < dp[j] + 1:
                 dp[i] = dp[j] + 1
+                lis[i] = lis[j] + [arr[i]]
     
     lis_len = max(dp)
-    lis = []
-    t = lis_len
-    for i in range(N - 1, -1, -1):
-        if dp[i] == t:
-            lis.append(arr[i])
-            t -= 1
-            if t == 0:
-                break
-    lis.reverse()
-    
+    lis = lis[dp.index(lis_len)]    
     sys.stdout.write(f'{lis_len}\n{" ".join(map(str, lis))}')
 
 
