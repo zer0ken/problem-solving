7ba22d3dab1ab9a7506f2d3f7e2c98d65691ba35
zer0ken <a37592777@gmail.com>
2025-03-24 10:20:36 +0900

    테스트

--- 테스트/test.py
+++ 테스트/test.py
@@ -1,27 +1,11 @@
-def main():
-    import sys
-    import math
-    
-    N, *abnormalities = map(int, sys.stdin.read().split())
-    acc_sum = [0]
-    for v in abnormalities:
-        acc_sum.append(acc_sum[-1] + v)
-    
-    immunity_gains = [abnormalities[i] - acc_sum[i] for i in range(N)]    
-    best_gains = [immunity_gains[0]]
-    for i in range(1, N):
-        best_gains.append(immunity_gains[i] if immunity_gains[i] > best_gains[-1] else best_gains[-1])
-    
-    immunity = 0
-    loops = 0
-    for i in range(N):
-        if immunity < acc_sum[i + 1]:
-            new_loops = math.ceil((acc_sum[i + 1] - immunity) / best_gains[i])
-            immunity += best_gains[i] * new_loops
-            loops += new_loops
-    
-    sys.stdout.write(str(loops))
-                
+import sys
 
-if __name__ == '__main__':
-    main()
+N_K, *lines = sys.stdin.read().rstrip().splitlines()
+N, K = map(int, N_K.split())
+knapsack = [0] * (K + 1)
+for line in lines:
+    W, V = map(int, line.split())
+    for i in range(K, W - 1, -1):
+        if knapsack[i] < knapsack[i-W] + V:
+            knapsack[i] = knapsack[i-W] + V
+sys.stdout.write(str(max(knapsack)))
