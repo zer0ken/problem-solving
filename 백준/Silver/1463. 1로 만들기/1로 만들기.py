20d1554243204f8f1e5b6cc92b626c4a99e5d925
zer0ken <a37592777@gmail.com>
2025-03-27 15:56:19 +0900

    [Silver III] Title: 1로 만들기, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -1,16 +1,9 @@
-import sys
-
-N = int(input())
-
-if N == 1:
-    print('0')
-    exit(0)
-
 dp = {1: 0, 2: 1, 3: 1}
-
 def solve(x):
-    if x not in dp:
-        dp[x] = 1 + min(solve(x//3) + x%3, solve(x//2) + x%2)
+    global dp
+    if x in dp:
+        return dp[x]
+    dp[x] = 1 + min(solve(x // 3) + x % 3, solve(x // 2) + x % 2)
     return dp[x]
 
-print(solve(N))
+print(solve(int(input())))
