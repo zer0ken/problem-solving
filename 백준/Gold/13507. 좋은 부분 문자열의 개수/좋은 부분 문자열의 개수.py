9cc0f92612b9c81e7e38a49bf1629ec098ae6299
zer0ken <a37592777@gmail.com>
2025-03-28 20:54:50 +0900

    [Gold I] Title: 좋은 부분 문자열의 개수, Time: 772 ms, Memory: 530760 KB -BaekjoonHub

--- 백준/Gold/13507. 좋은 부분 문자열의 개수/좋은 부분 문자열의 개수.py
+++ 백준/Gold/13507. 좋은 부분 문자열의 개수/좋은 부분 문자열의 개수.py
@@ -1,24 +1,27 @@
-import sys
+def main():
+    import sys
 
-S, BADS, K = sys.stdin.read().split()
-K = int(K)
-ord_a = ord('a')
+    S, BADS, K = sys.stdin.read().split()
+    K = int(K)
+    ord_a = ord('a')
 
-prefix_sum = [0]
-for cha in S:
-    prefix_sum.append(prefix_sum[-1] + int(BADS[ord(cha) - ord_a] == '0'))
+    prefix_sum = [0]
+    for cha in S:
+        prefix_sum.append(prefix_sum[-1] + int(BADS[ord(cha) - ord_a] == '0'))
+
+    trie = {}
+    count = 0
+    for i in range(len(S)):
+        cur = trie
+        for j in range(i + 1, len(S) + 1):
+            if prefix_sum[j] - prefix_sum[i] > K:
+                break
+            if S[j-1] not in cur:
+                cur[S[j-1]] = {}
+                count += 1
+            cur = cur[S[j-1]]
+
+    sys.stdout.write(str(count))
 
 
-trie = {}
-count = 0
-for i in range(len(S)):
-    cur = trie
-    for j in range(i + 1, len(S) + 1):
-        if prefix_sum[j] - prefix_sum[i] > K:
-            break
-        if S[j-1] not in cur:
-            cur[S[j-1]] = {}
-            count += 1
-        cur = cur[S[j-1]]
-
-print(count)
+main()
