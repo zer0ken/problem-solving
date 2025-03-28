f35b1b8f8d378817f5f7ed887f85ad20b90bcf5b
zer0ken <a37592777@gmail.com>
2025-03-29 01:51:06 +0900

    [Platinum I] Title: 교차 선분, Time: 300 ms, Memory: 47056 KB -BaekjoonHub

--- 백준/Platinum/2474. 교차 선분/교차 선분.py
+++ 백준/Platinum/2474. 교차 선분/교차 선분.py
@@ -4,37 +4,34 @@ def main():
     readline = sys.stdin.readline
     write = sys.stdout.write
     
-    
     N = int(readline())
-    edges = [tuple(map(int, readline().split())) for i in range(N)]
-    edges.sort()
+    segments = [tuple(map(int, readline().split())) for _ in range(N)]
+    segments.sort()
     
-    edges1 = []
-    edges2 = []
+    X = []
+    Y = []
     
     max_end = -1
-    for i, (start, end) in enumerate(edges):
-        if max_end > end:
-            edges2.append(i)
+    for i, segment in enumerate(segments):
+        if max_end > segment[1]:
+            Y.append(segment)
         else:
-            edges1.append(i)
-            max_end = end
+            X.append(segment)
+            max_end = segment[1]
     
     
     def is_crossing(p1, p2):
-        nonlocal edges, edges1, edges2
+        nonlocal segments, X, Y
         
-        s1, e1 = edges[edges1[p1]]
-        s2, e2 = edges[edges2[p2]]
-        return s1 < s2 and e1 > e2 or s1 > s2 and e1 < e2        
+        return (X[p1][0] - Y[p2][0]) * (X[p1][1] - Y[p2][1]) < 0
     
     
     def get_chain(p1, p2, looking_edges2):
-        nonlocal edges1, edges2
+        nonlocal X, Y
         
         chain = 2
-        while p1 < len(edges1) and p2 < len(edges2):
-            if looking_edges2 and p2 < len(edges2) - 1:
+        while p1 < len(X) and p2 < len(Y):
+            if looking_edges2 and p2 < len(Y) - 1:
                 if not is_crossing(p1, p2):
                     p1 -= 1
                     break
@@ -42,11 +39,11 @@ def main():
                     p2 += 1
                     chain += 1
                     looking_edges2 = not looking_edges2
-                elif p1 < len(edges1) - 1:
+                elif p1 < len(X) - 1:
                     p1 += 1
                 else:
                     break
-            elif not looking_edges2 and p1 < len(edges1) - 1:
+            elif not looking_edges2 and p1 < len(X) - 1:
                 if not is_crossing(p1, p2):
                     p2 -= 1
                     break
@@ -54,7 +51,7 @@ def main():
                     p1 += 1
                     chain += 1
                     looking_edges2 = not looking_edges2
-                elif p2 < len(edges2) - 1:
+                elif p2 < len(Y) - 1:
                     p2 += 1
                 else:
                     break
@@ -66,30 +63,33 @@ def main():
     
     max_chain = 1
     p1 = p2 = 0
-    while p1 < len(edges1) and p2 < len(edges2) and N - p1 - p2 > max_chain:
+    while p1 < len(X) and p2 < len(Y) and N - p1 - p2 > max_chain:
         if is_crossing(p1, p2):
             chain, p1, p2 = get_chain(p1, p2, looking_edges2=False)
-            max_chain = max(max_chain, chain)
+            if max_chain < chain:
+                max_chain = chain
             p1 += 1
             p2 += 1
-        elif edges1[p1] < edges2[p2]:
+        elif X[p1][0] < Y[p2][0]:
             p1 += 1
         else:
             p2 += 1
     
     p1 = p2 = 0
-    while p1 < len(edges1) and p2 < len(edges2) and N - p1 - p2 > max_chain:
+    while p1 < len(X) and p2 < len(Y) and N - p1 - p2 > max_chain:
         if is_crossing(p1, p2):
             chain, p1, p2 = get_chain(p1, p2, looking_edges2=True)
-            max_chain = max(max_chain, chain)
+            if max_chain < chain:
+                max_chain = chain
             p1 += 1
             p2 += 1
-        elif edges1[p1] < edges2[p2]:
+        elif X[p1][0] < Y[p2][0]:
             p1 += 1
         else:
             p2 += 1
     
     write(str(max_chain))
+
 
 if __name__ == '__main__':
     main()
