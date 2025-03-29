699460f70fe9b5c42fce10e41ba2c2152b2df8f5
zer0ken <a37592777@gmail.com>
2025-03-30 00:59:36 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 퍼즐 게임 챌린지, Time: 1234.52 ms, Memory: 34.1 MB -BaekjoonHub

--- 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.py
+++ 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.py
@@ -1,18 +1,15 @@
 def solution(diffs, times, limit):
-    
     lo = 1
     hi = max(diffs)
     
     while lo <= hi:
         mid = (lo + hi) // 2
-        total_time = 0
         
+        total_time = 0
         for i, (diff, time) in enumerate(zip(diffs, times)):
-            fails = max(0, diff - mid)
-            if fails:
-                total_time += fails * (time + times[i-1])
             total_time += time
-            
+            if diff > mid:
+                total_time += (diff - mid) * (time + times[i-1])
             if total_time > limit:
                 break
         
