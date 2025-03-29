123e08b0c9d8a4ebd46186dc2509905ab8a59d49
zer0ken <a37592777@gmail.com>
2025-03-30 02:06:41 +0900

    [Silver I] Title: 소수&팰린드롬, Time: 120 ms, Memory: 110736 KB -BaekjoonHub

--- 백준/Silver/1747. 소수＆팰린드롬/소수＆팰린드롬.py
+++ 백준/Silver/1747. 소수＆팰린드롬/소수＆팰린드롬.py
@@ -1,27 +1,29 @@
-import sys
+def main():
+    import sys
+
+    def is_prime(n):
+        d = 2
+        while d * d <= n:
+            if n % d == 0:
+                return False
+            d += 1
+        return True
+
+    def is_palindrome(str_n):
+        for i in range(len(str_n) // 2):
+            if str_n[i] != str_n[-(i + 1)]:
+                return False
+        return True
+
+    N = int(sys.stdin.readline().rstrip())
+    n = N if N > 2 else 2
+    while True:
+        if is_palindrome(str(n)) and is_prime(n):
+            break
+        n += 1
+
+    sys.stdout.write(str(n))
 
 
-def is_prime(n):
-    d = 2
-    while d * d <= n:
-        if n % d == 0:
-            return False
-        d += 1
-    return True
-
-
-def is_palindrome(str_n):
-    for i in range(len(str_n) // 2):
-        if str_n[i] != str_n[-(i + 1)]:
-            return False
-    return True
-
-
-N = int(sys.stdin.readline().rstrip())
-n = N if N > 2 else 2
-while True:
-    if is_palindrome(str_n := str(n)) and is_prime(n):
-        break
-    n += 1
-
-sys.stdout.write(str_n)
+if __name__ == '__main__':
+    main()
