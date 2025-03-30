59000c4cce134c57548022a8c1bb68ae6163a620
zer0ken <a37592777@gmail.com>
2025-03-30 21:04:42 +0900

    [Silver III] Title: 게임, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1072. 게임/게임.py
+++ 백준/Silver/1072. 게임/게임.py
@@ -1,16 +1,7 @@
 X, Y = map(int, input().split())
-
 Z = Y*100 // X
 if Z >= 99:
     print('-1')
-    exit(0)
-
-lo, hi = 1, X
-while lo <= hi:
-    mid = (lo + hi) // 2
-    if (Y + mid)*100 // (X + mid) == Z:
-        lo = mid + 1
-    else:
-        hi = mid - 1
-
-print(lo)
+else:
+    extra_game = (100*Y - (Z + 1)*X) / (Z - 99)
+    print(int(extra_game) if int(extra_game) == extra_game else int(extra_game) + 1)
