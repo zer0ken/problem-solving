4e48a0d964e9f7d9a85045f63706640c01383b2f
zer0ken <a37592777@gmail.com>
2025-04-01 16:34:23 +0900

    [Bronze I] Title: 0의 개수, Time: 1056 ms, Memory: 94540 KB -BaekjoonHub

--- 백준/Bronze/11170. 0의 개수/0의 개수.py
+++ 백준/Bronze/11170. 0의 개수/0의 개수.py
@@ -1,6 +1,3 @@
 for _ in range(int(input())):
     N, M = map(int, input().split())
-    zeros = 0
-    for x in range(N, M + 1):
-        zeros += str(x).count('0')
-    print(zeros)
+    print(''.join(map(str, range(N, M + 1))).count('0'))
