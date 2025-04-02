d0ab62e90c6b87cbc32c1d23df28b9186ca91d18
zer0ken <a37592777@gmail.com>
2025-04-03 03:44:52 +0900

    [Silver II] Title: DFS와 BFS, Time: 44 ms, Memory: 33528 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,41 +1,38 @@
 import sys
 
-input = sys.stdin.readline
+N, M, V = map(int, sys.stdin.readline().split())
+adj = [[] for _ in range(N + 1)]
+for line in range(M):
+    a, b = map(int, sys.stdin.readline().split())
+    adj[a].append(b)
+    adj[b].append(a)
 
+visited = [False] * (N + 1)
+results = []
 
-def dfs(v):
-    vstd[v] = True
-    smap[v].sort()
-    result.append(v)
-    for i in smap[v]:
-        if not vstd[i]:
-            dfs(i)
-
-def bfs(v):
-    queue = [v]
-    vstd[v] = True
-    result.append(v)
-    while queue:
-        d = queue.pop(0)
-        for i in smap[d]:
-            if not vstd[i]:
-                queue.append(i)
-                vstd[i] = True
-                result.append(i)
-
-N,M,V = map(int,input().split())
-smap = [[] for i in range(N+1)]
-vstd = [False] * (N+1)
-result = []
-for i in range(M):
-    a,b = map(int, input().split())
-    smap[a].append(b)
-    smap[b].append(a)
+def dfs(cur):
+    visited[cur] = True
+    results.append(cur)
+    adj[cur].sort()
+    for next in adj[cur]:
+        if not visited[next]:
+            dfs(next)
 
 dfs(V)
-print(*result)
-result = []
-cnt = 1
-vstd = [False] * (N+1)
-bfs(V)
-print(*result)
+print(*results)
+
+results = []
+visited = [False] * (N + 1)
+visited[V] = 1
+queue = [V]
+
+while queue:
+    cur = queue.pop(0)
+    results.append(cur)
+    
+    for next in adj[cur]:
+        if not visited[next]:
+            visited[next] = True
+            queue.append(next)
+
+print(*results)
