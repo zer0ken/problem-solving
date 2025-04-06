44e7e75201b1ddd70f9975f4aa64ab18e347d462
zer0ken <a37592777@gmail.com>
2025-04-06 15:45:41 +0900

    [Silver I] Title: 단지번호붙이기, Time: 36 ms, Memory: 32536 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,31 +1,39 @@
-import sys
-from collections import deque
+def main():
+    import sys
+    
+    N, *board = sys.stdin.read().split()
+    N = int(N)
+    
+    deltas = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+    visited = [[False] * N for _ in range(N)]
+    count = []
+    
+    def dfs(row, col, value):
+        if row < 0 or row >= N or \
+                col < 0 or col >= N or \
+                visited[row][col] or \
+                board[row][col] != value:
+            return
+        
+        visited[row][col] = True
+        if value != '0':
+            count[-1] += 1
+            
+        for drow, dcol in deltas:
+            dfs(row + drow, col + dcol, value)
 
-input = sys.stdin.readline
-N = int(input())
-board = [list(map(lambda x: x == '1', input().rstrip())) for _ in range(N)]
-visited = [[False] * N for _ in range(N)]
-deltas = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+    for row in range(N):
+        for col in range(N):
+            if visited[row][col]:
+                continue
+            value = board[row][col]
+            if value != '0':
+                count.append(0)
+            dfs(row, col, board[row][col])
+    count.sort()
+    
+    print(len(count), *count, sep='\n')
 
-groups = []
-for i in range(N):
-    for j in range(N):
-        if visited[i][j] or not board[i][j]:
-            continue
-        visited[i][j] = True
-        count = 0
-        queue = deque([(i, j)])
-        while queue:
-            row, col = queue.popleft()
-            count += 1
-            for drow, dcol in deltas:
-                nrow = row + drow
-                ncol = col + dcol
-                if nrow < 0 or nrow >= N or ncol < 0 or ncol >= N or visited[nrow][ncol] or not board[nrow][ncol]:
-                    continue
-                visited[nrow][ncol] = True
-                queue.append((nrow, ncol))
-        groups.append(count)
-groups.sort()
-sys.stdout.write(f'{len(groups)}\n')
-sys.stdout.write('\n'.join(map(str, groups)))
+
+if __name__ == '__main__':
+    main()
