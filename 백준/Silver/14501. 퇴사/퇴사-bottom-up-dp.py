837852feec43590d9fb085acdf4a08e88a821719
zer0ken <a37592777@gmail.com>
2025-04-07 20:43:46 +0900

    풀이 추가

---
+++ 백준/Silver/14501. 퇴사/퇴사-bottom-up-dp.py
@@ -0,0 +1,19 @@
+def main():
+    import sys
+    
+    N, *lines = sys.stdin.read().rstrip().splitlines()
+    N = int(N)
+    
+    dp = [0] * (N + 1)
+    
+    for i, line in enumerate(lines):
+        T, P = map(int, line.split())
+        if i + T > N:
+            continue
+        dp[i+T] = max(dp[i+T], max(dp[:i+1]) + P)
+    
+    print(dp[-1])
+
+
+if __name__ == '__main__':
+    main()
