403665a124c125761a6e4bcfd8448656b7c3a1aa
zer0ken <a37592777@gmail.com>
2025-04-07 20:43:09 +0900

    [Silver III] Title: 퇴사, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.py
+++ 백준/Silver/14501. 퇴사/퇴사.py
@@ -3,31 +3,16 @@ def main():
     
     N, *lines = sys.stdin.read().rstrip().splitlines()
     N = int(N)
-    counsels = [[] for _ in range(N + 1)]
+    
+    dp = [0] * (N + 1)
+    
     for i, line in enumerate(lines):
         T, P = map(int, line.split())
-        if i + T <= N:
-            counsels[i+T].append([i, P])
+        if i + T > N:
+            continue
+        dp[i+T] = max(dp[i+T], max(dp[:i+1]) + P)
     
-    dp = [None] * (N + 1)
-    
-    def evaluate(end_day):
-        nonlocal dp
-        
-        if end_day < 0:
-            return 0
-        
-        if dp[end_day] is not None:
-            return dp[end_day]
-        
-        total_paid = evaluate(end_day - 1)
-        for start_day, paid in counsels[end_day]:
-            total_paid = max(total_paid, paid + evaluate(start_day))
-        
-        dp[end_day] = total_paid
-        return dp[end_day]
-    
-    print(evaluate(N))
+    print(max(dp))
 
 
 if __name__ == '__main__':
