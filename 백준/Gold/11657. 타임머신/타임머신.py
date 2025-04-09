550d6f502761cef0dab9f40e097e4f78fe90bc11
zer0ken <a37592777@gmail.com>
2025-04-09 21:58:59 +0900

    [Gold IV] Title: 타임머신, Time: 456 ms, Memory: 34456 KB -BaekjoonHub

---
+++ 백준/Gold/11657. 타임머신/타임머신.py
@@ -0,0 +1,25 @@
+def main():
+    import sys
+    
+    lines = iter(sys.stdin.read().rstrip().splitlines())
+    
+    N, M = map(int, next(lines).split())
+    edges = [list(map(int, line.split())) for line in lines]
+    
+    INF = float('inf')
+    dist = [INF] * (N + 1)
+    dist[1] = 0
+    
+    for i in range(N):
+        for start, end, d in edges:
+            if dist[end] > dist[start] + d:
+                if i == N - 1:
+                    print('-1')
+                    exit(0)
+                dist[end] = dist[start] + d
+    
+    print(*(-1 if d >= INF else d for d in dist[2:]), sep='\n')
+
+
+if __name__ == '__main__':
+    main()
