02832df3f5a38ec1e9c54480668ccd4fda0e5b01
zer0ken <a37592777@gmail.com>
2025-04-09 22:03:40 +0900

    [Gold IV] Title: 타임머신, Time: 156 ms, Memory: 111956 KB -BaekjoonHub

--- 백준/Gold/11657. 타임머신/타임머신.py
+++ 백준/Gold/11657. 타임머신/타임머신.py
@@ -10,11 +10,15 @@ dist = [INF] * (N + 1)
 dist[1] = 0
 
 for i in range(N):
+    changed = 0
     for start, end, d in edges:
         if dist[end] > dist[start] + d:
             if i == N - 1:
                 print('-1')
                 exit(0)
+            changed = 1
             dist[end] = dist[start] + d
+    if not changed:
+        break
 
 print(*(-1 if d >= INF else d for d in dist[2:]), sep='\n')
