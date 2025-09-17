5777cd8d5a254ecd2d064e5225cd578519e7c5c2
zer0ken <a37592777@gmail.com>
2025-09-17 19:57:41 +0900

    [level 3] Title: 베스트앨범, Time: 0.09 ms, Memory: 9.13 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -1,16 +1,30 @@
 def solution(genres, plays):
-    import heapq
-    from collections import defaultdict
+    sum_genre_plays = {}
+    genre_ids = {}
     
-    genre_plays = defaultdict(int)
-    genre_songs = defaultdict(list)
     for i, (g, p) in enumerate(zip(genres, plays)):
-        genre_plays[g] += p
-        heapq.heappush(genre_songs[g], (-p, i))
+        if g not in sum_genre_plays:
+            sum_genre_plays[g] = p
+            
+            genre_ids[g] = [i]
+            
+        else:
+            sum_genre_plays[g] += p
+            
+            if len(genre_ids[g]) == 1:
+                if plays[genre_ids[g][0]] < p:
+                    genre_ids[g].insert(0, i)
+                else:
+                    genre_ids[g].append(i)
+            elif len(genre_ids[g]) == 2:
+                if plays[genre_ids[g][0]] < p:
+                    genre_ids[g] = [i, genre_ids[g][0]]
+                elif plays[genre_ids[g][1]] < p:
+                    genre_ids[g] = [genre_ids[g][0], i]
     
-    playlist = []
-    for g in sorted(genre_plays.keys(), key=lambda g: -genre_plays[g]):
-        for _ in range(min(2, len(genre_songs[g]))):
-            playlist.append(heapq.heappop(genre_songs[g])[1])
-    
-    return playlist
+    result = []
+    genre_keys = list(sum_genre_plays.keys())
+    genre_keys.sort(key=lambda g: sum_genre_plays[g], reverse=True)
+    for g in genre_keys:
+        result.extend(genre_ids[g])
+    return result
